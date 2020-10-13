10895fcac36b5559ab027dac2947a22b433e3d11
simofleanta <simo.fleanta@gmail.com>
2020-10-13 17:05:23 +0200

    s

--- happy.py
+++ happy.py
@@ -4,6 +4,7 @@ import seaborn as sns
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 import plotly.express as px
+import numpy as np
 import plotly
 
 
@@ -13,6 +14,9 @@ print(happy.columns)
 df=DataFrame(happy)
 print(df.head(3))
 
+#Charts_for_all_df
+
+#violinchart
 
 sns.violinplot(x=df["Freedom_to_make_life_choices"], y=df["Regional_indicator"], color='purple')
 plt.show()
@@ -22,19 +26,11 @@ plt.show()
 
 Scatter = sns.lmplot(data=df, x='Explained_by_Log_GDP_per_capita', y='Social_support',
                  fit_reg=False)
+plt.show()
 
 vis=sns.boxplot(data=df, x='Freedom_to_make_life_choices',y='Regional_indicator', palette='Reds')
 plt.show()
 
-
-#rownumbers
-
-Row_numbers, Column_numbers = df.shape
-print("The number of countries in the dataset =",Row_numbers)
-print("The number of parameters for happiness =",Column_numbers)
-
-
-"""In order to obtain charts more global regions will be extracted"""
 #--------------------------WestEu-----------------------#
 
 
@@ -65,17 +61,13 @@ title='Happiness_overview',
   
 )
 
-#setting a layout(optional task)
-
-
-
 fig.update_layout(
     title_font_size=42,
     title_font_family='Arial'
 )
-
+plotly.offline.plot(fig, filename='Healthy_life_expectancy in West Eu')
 fig.show()
-
+#---------------------------------------------------------
 #perception on generosity treemap
 
 Country=WestEu['Country_name']
@@ -91,8 +83,6 @@ hover_name=Generosity
   
 )
 
-#setting a layout(optional task)
-
 fig.update_layout(
     title_font_size=42,
     title_font_family='Arial'
@@ -101,13 +91,50 @@ fig.update_layout(
 plotly.offline.plot(fig, filename='Generosity perception in Central Eu')
 fig.show()
 
+#--------------------------------------------------------------------------------------------
+#SUNBURST - Freedom_to_make_life_choices
 
+fig = px.sunburst(df, path=['Regional_indicator','Country_name'], values='Freedom_to_make_life_choices',
+                  color='Freedom_to_make_life_choices', hover_data=['Freedom_to_make_life_choices'],
+                  color_continuous_scale='blues',
+                  color_continuous_midpoint=np.average(df['Freedom_to_make_life_choices'], weights=df['Freedom_to_make_life_choices']))
 
-#pieChart
+plotly.offline.plot(fig, filename='sun')
 
-fig = px.pie(WestEu, values='Freedom_to_make_life_choices', names='Country_name', title='Region-wise economy',height=550)
 fig.show()
 
+#SUNBURST - Perceptions_of_corruption
+
+fig = px.sunburst(df, path=['Regional_indicator','Country_name'], values='Perceptions_of_corruption',
+                  color='Perceptions_of_corruption', hover_data=['Perceptions_of_corruption'],
+                  color_continuous_scale='bluered',
+                  color_continuous_midpoint=np.average(df['Perceptions_of_corruption'], weights=df['Perceptions_of_corruption']))
+
+plotly.offline.plot(fig, filename='sun')
+
+fig.show()
+
+#SUNBURST - Logged_GDP_perCapita
+
+fig = px.sunburst(df, path=['Regional_indicator','Country_name'], values='Logged_GDP_perCapita',
+                  color='Logged_GDP_perCapita', hover_data=['Logged_GDP_perCapita'],
+                  color_continuous_scale='portland',
+                  color_continuous_midpoint=np.average(df['Logged_GDP_perCapita'], weights=df['Logged_GDP_perCapita']))
+
+plotly.offline.plot(fig, filename='sun')
+
+fig.show()
+
+#SUNBURST - Healthy_life_expectancy
+
+fig = px.sunburst(df, path=['Regional_indicator','Country_name'], values='Healthy_life_expectancy',
+                  color='Healthy_life_expectancy', hover_data=['Healthy_life_expectancy'],
+                  color_continuous_scale='blues',
+                  color_continuous_midpoint=np.average(df['Healthy_life_expectancy'], weights=df['Healthy_life_expectancy']))
+
+plotly.offline.plot(fig, filename='sun')
+
+fig.show()
 
 
 #--------------------------------SouthEastAsia-----------------------------------------------
@@ -135,7 +162,7 @@ plt.show()
 sns.pairplot(Central_and_Eastern_Europe, vars=['Freedom_to_make_life_choices','Logged_GDP_perCapita'])
 plt.show()
 
-#merge data sets 
+
 
 
 
