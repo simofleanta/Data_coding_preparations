3407d30816c3751b0f8a63ab57044c8f1df645f7
simofleanta <simo.fleanta@gmail.com>
2020-10-14 16:42:43 +0200

    s

--- German_elections.py
+++ German_elections.py
@@ -9,6 +9,36 @@ import plotly
 
 
 #open the file
+"""g=pd.read_csv('2017_german_election_party.csv')
+print(g.columns)
+df=DataFrame(g)
+
+print(df.head(3))
+
+area=df['area_name']
+state=df['state']
+party=df['party']
+votes1=df['votes_first_vote']
+votes2=df['votes_second_vote']
+area_id=['area_id']
+
+fig = go.Figure(data=go.Heatmap(
+                   z=votes2,
+                   x=state,
+                   y=party,
+                   colorscale='Viridis'))
+
+fig.update_layout(
+    title='Votes in Germany 2017',
+    xaxis_nticks=18)
+
+
+plotly.offline.plot(fig, filename='votes_g')
+
+fig.show()"""
+
+#elections overall g
+
 g=pd.read_csv('2017_german_election_party.csv')
 print(g.columns)
 df=DataFrame(g)
@@ -21,3 +51,4 @@ party=df['party']
 votes1=df['votes_first_vote']
 votes2=df['votes_second_vote']
 area_id=['area_id']
+
