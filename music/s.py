637cb9804bf13a67939ce82b28fc9d4609bd8e39
simofleanta <simo.fleanta@gmail.com>
2020-10-16 13:28:40 +0200

    s

--- music/s.py
+++ music/s.py
@@ -8,13 +8,13 @@ import numpy as np
 import plotly
 
 #open the file
-s=pd.read_csv('data_by_artist.csv')
+s=pd.read_csv('data_by_genres.csv')
 print(s.columns)
 df=DataFrame(s)
 
 print(df.head(3))
 
-artists=df['artists']
+genres=df['genres']
 popularity=df['popularity']
 duration_ms=df['duration_ms']
 acousticness=df['acousticness']
@@ -27,10 +27,46 @@ speechiness=df['speechiness']
 valence=df['valence']
 tempo=df['tempo']
 danceability=df['danceability']
-count=df['count']
+
+
+fig = px.scatter(df, x="genres", y="popularity", color="liveness",
+                 size='duration_ms', hover_data=['popularity'],
+                 color_continuous_scale='RdBu')
+
+plotly.offline.plot(fig, filename='m')
+
+#
+
+
+fig = px.scatter(df, x="genres", y="liveness", color="popularity",
+                 size='duration_ms', hover_data=['popularity'],
+                 color_continuous_scale='Sunset')
+
+plotly.offline.plot(fig, filename='m')
+
+#
+
+fig=px.treemap(df,
+path=[genres],
+values=liveness,
+color=popularity,
+color_continuous_scale='RdYlBu',
+title='genres popularity',
+hover_name=genres
+  
+)
+
+fig.update_layout(
+    title_font_size=42,
+    title_font_family='Arial'
+)
+
+plotly.offline.plot(fig, filename='Music perception')
 
 
 
-sns.violinplot(x=df["popularity"], y=df["artists"], color='purple')
-plt.show()
+
+
+
+
 
