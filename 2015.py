cebeabda83d3adbfe40b9b504ddb12e0a6f1612b
simofleanta <simo.fleanta@gmail.com>
2020-10-19 09:02:33 +0200

    s

--- 2015.py
+++ 2015.py
@@ -16,10 +16,35 @@ df=DataFrame(h)
 print(df.head(3))
 
 
-fig = px.sunburst(df, path=['Region','Happiness Rank','Country'], values='Happiness Score',
+"""fig = px.sunburst(df, path=['Region','Happiness Rank','Country'], values='Happiness Score',
                   color='Happiness Score', hover_data=['Happiness Score'],
                   color_continuous_scale='blues',
                   color_continuous_midpoint=np.average(df['Happiness Score'], weights=df['Happiness Score']))
 
-plotly.offline.plot(fig, filename='hap')
+plotly.offline.plot(fig, filename='hap')"""
+
+#treemap happiness indicators per global region
+
+rank=df['Happiness Rank']
+Score=df['Happiness Score']
+StandardErr=df['Standard Error']
+Generosity=df['Generosity']
+Region=df['Region']
+
+fig=px.treemap(df,
+path=[Region],
+values=rank,
+color=rank,
+color_continuous_scale='RdBu',
+title='Happiness',
+  
+)
+
+fig.update_layout(
+    title_font_size=42,
+    title_font_family='Arial'
+)
+plotly.offline.plot(fig, filename='Happyness Rank')
+
+
 
