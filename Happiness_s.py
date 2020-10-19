251693ebd8e8167cdd8272e6e3bdece89c12071a
simofleanta <simo.fleanta@gmail.com>
2020-10-19 18:15:54 +0200

    add heat correlations

--- Happiness_s.py
+++ Happiness_s.py
@@ -17,27 +17,27 @@ print(df.head(3))
 
 #happiness Score per regions
 
-"""df_scores=df.groupby(['Region'])['Happiness Score'].mean()
+df_scores=df.groupby(['Region'])['Happiness Score'].mean()
 df_scores=pd.DataFrame(data=df_scores)
 df_h_scores=df_scores.sort_values(by='Happiness Score',ascending=False,axis=0)
 print(df_h_scores)
 
 fig = px.bar(df_h_scores, x="Happiness Score", y=df_h_scores.index, color='Happiness Score',color_continuous_scale='Sunset',title="Happiness in different regions")
-plotly.offline.plot(fig, filename='hap')"""
+plotly.offline.plot(fig, filename='hap')
 
 #happiness rank per regions
 
-"""df_scores=df.groupby(['Region'])['Happiness Rank'].mean()
+df_scores=df.groupby(['Region'])['Happiness Rank'].mean()
 df_scores=pd.DataFrame(data=df_scores)
 df_h_rank=df_scores.sort_values(by='Happiness Rank',ascending=False,axis=0)
 print(df_h_rank)
 
 fig = px.bar(df_h_rank, x="Happiness Rank", y=df_h_rank.index, color='Happiness Rank',color_continuous_scale='Sunset',title="Happiness rank in different regions")
-plotly.offline.plot(fig, filename='hap')"""
+plotly.offline.plot(fig, filename='hap')
 
 #happiness/rank WE
 
-"""WE=df[df.Region =='Western Europe']
+WE=df[df.Region =='Western Europe']
 Country=WE['Country']
 print(WE)
 
@@ -47,7 +47,7 @@ Happiness_rank=WE_happiness.sort_values(by='Happiness Rank',ascending=False,axis
 print(Happiness_rank)
 
 fig = px.bar(Happiness_rank, x="Happiness Rank", y=Happiness_rank.index, color='Happiness Rank',color_continuous_scale='Sunset',title="Happiness rank in different regions")
-plotly.offline.plot(fig, filename='hap')"""
+plotly.offline.plot(fig, filename='hap')
 
 # bubble chart rank in WE
 
@@ -71,3 +71,8 @@ fig = go.Figure(data=[go.Scatter(
 ])
 
 plotly.offline.plot(fig, filename='m')
+
+
+"""#creating new datasets comprising of below mentioned columns
+x1=y1_df[['Generosity','Family','Score']].copy()
+x2=y2_df[['Generosity','Social support','Score']].copy()"""
---
+++ heat_map.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import plotly
+
+
+#open the file
+h=pd.read_csv('2015.csv')
+print(h.columns)
+df=DataFrame(h)
+
+print(df.head(3))
+
+#create a mini dataset from where to take desired columns to create corrs through heatmaps
+generosity=df[['Generosity','Family','Happiness Rank']].copy()
+freedom=df[['Freedom','Happiness Score','Generosity']].copy()
+
+heat1=generosity.corr()
+plt.figure(figsize=(10,6))
+sns.heatmap(heat1,annot=True, cmap='RdPu')
+plt.show()
+
+heat2=generosity.corr()
+plt.figure(figsize=(10,6))
+sns.heatmap(heat1,annot=True, cmap='Blues')
+plt.show()
+
+
+
+
+
+
+
