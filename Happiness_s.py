c6be4f568985535f627f3ed8a9008d792dc62751
simofleanta <simo.fleanta@gmail.com>
2020-10-19 17:22:30 +0200

    s

--- Happiness_s.py
+++ Happiness_s.py
@@ -27,25 +27,24 @@ plotly.offline.plot(fig, filename='hap')"""
 
 #happiness rank per regions
 
-df_scores=df.groupby(['Region'])['Happiness Rank'].mean()
+"""df_scores=df.groupby(['Region'])['Happiness Rank'].mean()
 df_scores=pd.DataFrame(data=df_scores)
 df_h_rank=df_scores.sort_values(by='Happiness Rank',ascending=False,axis=0)
 print(df_h_rank)
 
 fig = px.bar(df_h_rank, x="Happiness Rank", y=df_h_rank.index, color='Happiness Rank',color_continuous_scale='Sunset',title="Happiness rank in different regions")
-plotly.offline.plot(fig, filename='hap')
+plotly.offline.plot(fig, filename='hap')"""
 
-
-
-
-
-
+#happiness/rank WE
 
 WE=df[df.Region =='Western Europe']
 Country=WE['Country']
 print(WE)
 
-WE_fam=WE.groupby(['Country'])['Family'].mean()
-WE_fam=pd.DataFrame(data=WE_fam)
-Family_c=WE_fam.sort_values(by='Family',ascending=False,axis=0)
-print(Family_c)
+WE_happiness=WE.groupby(['Country'])['Happiness Rank'].mean()
+WE_happiness=pd.DataFrame(data=WE_happiness)
+Happiness_rank=WE_happiness.sort_values(by='Happiness Rank',ascending=False,axis=0)
+print(Happiness_rank)
+
+fig = px.bar(Happiness_rank, x="Happiness Rank", y=Happiness_rank.index, color='Happiness Rank',color_continuous_scale='Sunset',title="Happiness rank in different regions")
+plotly.offline.plot(fig, filename='hap')
