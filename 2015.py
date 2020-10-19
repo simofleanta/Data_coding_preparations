697c3594a0116dd7a222e240e17c17bd7fbd6f83
simofleanta <simo.fleanta@gmail.com>
2020-10-19 11:36:29 +0200

    update

--- 2015.py
+++ 2015.py
@@ -16,13 +16,6 @@ df=DataFrame(h)
 print(df.head(3))
 
 
-"""fig = px.sunburst(df, path=['Region','Happiness Rank','Country'], values='Happiness Score',
-                  color='Happiness Score', hover_data=['Happiness Score'],
-                  color_continuous_scale='blues',
-                  color_continuous_midpoint=np.average(df['Happiness Score'], weights=df['Happiness Score']))
-
-plotly.offline.plot(fig, filename='hap')"""
-
 #treemap on the lowest happiness rank per global region
 
 rank=df['Happiness Rank']
@@ -34,20 +27,11 @@ Country=df['Country']
 Scores=df['Happiness Score']
 
 
-"""fig=px.treemap(df,
-path=[Region],
-values=rank,
-color=rank,
-color_continuous_scale='RdBu',
-title='Happiness',
-  
-)
+fig = px.scatter(df, x="Region", y="Happiness Rank", color="Happiness Score",
+                 size='Family', hover_data=['Happiness Score'],
+                 color_continuous_scale='RdBu')
 
-fig.update_layout(
-    title_font_size=42,
-    title_font_family='Arial'
-)
-plotly.offline.plot(fig, filename='Happyness Rank')"""
+plotly.offline.plot(fig, filename='Happyness Rank')
 
 #bubblechart on scores and rank
 
@@ -70,14 +54,45 @@ fig = go.Figure(data=[go.Scatter(
 
 plotly.offline.plot(fig, filename='m')
 
+#bubble chart on countries in CE Europe
+CE=df[df.Region =='Central and Eastern Europe']
+data=CE
+Country=CE['Country']
+
+size = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100,110,120,130]
+fig = go.Figure(data=[go.Scatter(
+    x=Country, y=rank,
+    mode='markers',
+    marker=dict(size=size,
+    color=[110, 120, 130, 140, 150, 160,170, 180, 190, 200, 210, 220,230],
+        sizemode='area',
+        sizeref=2.*max(size)/(40.**2),
+        sizemin=4,
+        showscale=True)
+    )
+])
+
+plotly.offline.plot(fig, filename='m')
+
+# sunchart East Asia
+
+EA=df[df.Region =='Eastern Asia']
+dfS=EA
+Country=EA['Country']
+Freedom=EA['Freedom']
 
 
-# Scatter on happiness scores and rank based on global region with bubbles size on family
+fig = px.sunburst(dfS, path=['Region','Happiness Rank','Country'], values='Happiness Score',
+                  color='Happiness Score', hover_data=['Happiness Score'],
+                  color_continuous_scale='blues',
+                  color_continuous_midpoint=np.average(df['Happiness Score'], weights=df['Happiness Score']))
 
-"""fig = px.scatter(df, x="Region", y="Happiness Rank", color="Happiness Score",
-                 size='Family', hover_data=['Happiness Score'],
-                 color_continuous_scale='RdBu')
+plotly.offline.plot(fig, filename='hap')
 
-plotly.offline.plot(fig, filename='m')"""
+
+
+
+
+
 
 
