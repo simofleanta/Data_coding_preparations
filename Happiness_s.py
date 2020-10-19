48823e7f30ea1cfb615ae0167dd62ebfdda14460
simofleanta <simo.fleanta@gmail.com>
2020-10-19 17:15:46 +0200

    save

---
+++ Happiness_s.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import plotly
+
+
+#open the file
+h=pd.read_csv('2015.csv')
+print(h.columns)
+df=DataFrame(h)
+
+print(df.head(3))
+
+#happiness Score per regions
+
+"""df_scores=df.groupby(['Region'])['Happiness Score'].mean()
+df_scores=pd.DataFrame(data=df_scores)
+df_h_scores=df_scores.sort_values(by='Happiness Score',ascending=False,axis=0)
+print(df_h_scores)
+
+fig = px.bar(df_h_scores, x="Happiness Score", y=df_h_scores.index, color='Happiness Score',color_continuous_scale='Sunset',title="Happiness in different regions")
+plotly.offline.plot(fig, filename='hap')"""
+
+#happiness rank per regions
+
+df_scores=df.groupby(['Region'])['Happiness Rank'].mean()
+df_scores=pd.DataFrame(data=df_scores)
+df_h_rank=df_scores.sort_values(by='Happiness Rank',ascending=False,axis=0)
+print(df_h_rank)
+
+fig = px.bar(df_h_rank, x="Happiness Rank", y=df_h_rank.index, color='Happiness Rank',color_continuous_scale='Sunset',title="Happiness rank in different regions")
+plotly.offline.plot(fig, filename='hap')
+
+
+
+
+
+
+
+WE=df[df.Region =='Western Europe']
+Country=WE['Country']
+print(WE)
+
+WE_fam=WE.groupby(['Country'])['Family'].mean()
+WE_fam=pd.DataFrame(data=WE_fam)
+Family_c=WE_fam.sort_values(by='Family',ascending=False,axis=0)
+print(Family_c)
