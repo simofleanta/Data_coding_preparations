ab596571d281d5ba4a70a422f13c98f8a784bde8
simofleanta <simo.fleanta@gmail.com>
2020-10-19 16:24:39 +0200

    sav

--- GroupbyCountry.py
+++ GroupbyCountry.py
@@ -31,7 +31,7 @@ plotly.offline.plot(fig, filename='hap')"""
 
 #Family barchart in CE
 
-CE=df[df.Region =='Central and Eastern Europe']
+"""CE=df[df.Region =='Central and Eastern Europe']
 Country=CE['Country']
 print(CE)
 
@@ -41,7 +41,23 @@ Family_central=CE_fam.sort_values(by='Family',ascending=False,axis=0)
 print(Family_central)
 
 fig = px.bar(Family_central, x="Family", y=Family_central.index, color='Family',color_continuous_scale='Blues',title="Family in the Central Eastern")
+plotly.offline.plot(fig, filename='hap')"""
+
+#Australia and New Zealand family
+
+AZ=df[df.Region =='Australia and New Zealand']
+Country=AZ['Country']
+print(AZ)
+
+AZ_fam=AZ.groupby(['Country'])['Family'].mean()
+AZ_fam=pd.DataFrame(data=AZ_fam)
+Family_az=AZ_fam.sort_values(by='Family',ascending=False,axis=0)
+print(Family_az)
+
+fig = px.bar(Family_az, x="Family", y=Family_az.index, color='Family',color_continuous_scale='Blues',title="Family in the Australia NZ")
 plotly.offline.plot(fig, filename='hap')
+
+
 
 
 
