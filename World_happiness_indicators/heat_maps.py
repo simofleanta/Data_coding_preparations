00ca22d009ab197133b24bf0dc5b51e81efd7dea
simofleanta <simo.fleanta@gmail.com>
2020-10-20 16:32:07 +0200

    save

--- World_happiness_indicators/heat_maps.py
+++ World_happiness_indicators/heat_maps.py
@@ -23,6 +23,9 @@ print(WestEu)
 #heatmap matplotlib
 
 happy=WestEu[['Healthy_life_expectancy','Social_support','Perceptions_of_corruption','Country_name']].copy()
+economy_generosity=WestEu[['Country_name','Generosity','Logged_GDP_perCapita','Healthy_life_expectancy']].copy()
+
+#heat1
 Health=happy['Healthy_life_expectancy']
 Support=happy['Social_support']
 Corruption=happy['Perceptions_of_corruption']
@@ -31,3 +34,17 @@ Country=happy['Country_name']
 plt.figure(figsize=(5,5))
 sns.heatmap(happy.corr(), cmap='Blues')
 plt.show()
+
+#heat2
+Country=economy_generosity['Country_name']
+Health=economy_generosity['Healthy_life_expectancy']
+Generosity=economy_generosity['Generosity']
+Economy=economy_generosity['Logged_GDP_perCapita']
+
+plt.figure(figsize=(5,5))
+sns.heatmap(economy_generosity.corr(), cmap='Purples_r')
+plt.show()
+
+
+
+
