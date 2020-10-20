429b027efb76f0a6dd7400fc1a711f68e59b8efb
simofleanta <simo.fleanta@gmail.com>
2020-10-20 12:06:04 +0200

    s

--- 2015_happiness/tree.py
+++ 2015_happiness/tree.py
@@ -15,11 +15,18 @@ df=DataFrame(h)
 
 print(df.head(3))
 
-freedom2=df[['Freedom','Trust (Government Corruption)','Economy (GDP per Capita)','Happiness Rank']].copy()
-print(freedom2)
+freedom2=df[['Freedom','Region','Trust (Government Corruption)','Economy (GDP per Capita)','Happiness Rank']].copy()
+
 
 freedom=freedom2['Freedom']
 trust=freedom2['Trust (Government Corruption)']
 gdp=freedom2['Economy (GDP per Capita)']
 rank=freedom2['Happiness Rank']
+Region=freedom2['Region']
 
+
+
+region_gdp=freedom2.groupby(['Region'])['Economy (GDP per Capita)'].mean()
+gdp_r=pd.DataFrame(data=region_gdp)
+gdp_region=gdp_r.sort_values(by='Economy (GDP per Capita)',ascending=False,axis=0)
+print(gdp_region)
