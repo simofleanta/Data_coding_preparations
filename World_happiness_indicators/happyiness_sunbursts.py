630e24fb06ccc7b23a91f87248c95bb6c63ac34a
simofleanta <simo.fleanta@gmail.com>
2020-10-20 16:53:15 +0200

    save improvements

--- World_happiness_indicators/happyiness_sunbursts.py
+++ World_happiness_indicators/happyiness_sunbursts.py
@@ -14,51 +14,25 @@ print(happy.columns)
 df=DataFrame(happy)
 print(df.head(3))
 
-#Charts_for_all_df
 
-#violinchart
-
-sns.violinplot(x=df["Freedom_to_make_life_choices"], y=df["Regional_indicator"], color='purple')
-plt.show()
-
-sns.pairplot(df, vars=['Freedom_to_make_life_choices','Logged_GDP_perCapita'])
-plt.show()
-
-Scatter = sns.lmplot(data=df, x='Explained_by_Log_GDP_per_capita', y='Social_support',
-                 fit_reg=False)
-plt.show()
-
-vis=sns.boxplot(data=df, x='Freedom_to_make_life_choices',y='Regional_indicator', palette='Reds')
-plt.show()
-
-#--------------------------WestEu-----------------------#
-
-
-#extract global region
-WestEu=df[df.Regional_indicator=='Western_Europe']
-print(WestEu)
-
-
-
-
-
-#--------------------------------------------------------------------------------------------
 #SUNBURST - Freedom_to_make_life_choices
 
 fig = px.sunburst(df, path=['Regional_indicator','Country_name'], values='Freedom_to_make_life_choices',
                   color='Freedom_to_make_life_choices', hover_data=['Freedom_to_make_life_choices'],
                   color_continuous_scale='blues',
+                  title='Freedom to make life choices in the world',
                   color_continuous_midpoint=np.average(df['Freedom_to_make_life_choices'], weights=df['Freedom_to_make_life_choices']))
 
 plotly.offline.plot(fig, filename='sun')
 
 
-#df
+
 #SUNBURST - Perceptions_of_corruption
 
 fig = px.sunburst(df, path=['Regional_indicator','Country_name'], values='Perceptions_of_corruption',
                   color='Perceptions_of_corruption', hover_data=['Perceptions_of_corruption'],
                   color_continuous_scale='bluered',
+                  title='Perception of corruption in the world',
                   color_continuous_midpoint=np.average(df['Perceptions_of_corruption'], weights=df['Perceptions_of_corruption']))
 
 plotly.offline.plot(fig, filename='sun')
@@ -70,6 +44,7 @@ plotly.offline.plot(fig, filename='sun')
 fig = px.sunburst(df, path=['Regional_indicator','Country_name'], values='Logged_GDP_perCapita',
                   color='Logged_GDP_perCapita', hover_data=['Logged_GDP_perCapita'],
                   color_continuous_scale='portland',
+                  title='GDP per Capita acorss regions and countries',
                   color_continuous_midpoint=np.average(df['Logged_GDP_perCapita'], weights=df['Logged_GDP_perCapita']))
 
 plotly.offline.plot(fig, filename='sun')
@@ -81,38 +56,10 @@ plotly.offline.plot(fig, filename='sun')
 fig = px.sunburst(df, path=['Regional_indicator','Country_name'], values='Healthy_life_expectancy',
                   color='Healthy_life_expectancy', hover_data=['Healthy_life_expectancy'],
                   color_continuous_scale='blues',
+                  title='Healthy_life_expectancy',
                   color_continuous_midpoint=np.average(df['Healthy_life_expectancy'], weights=df['Healthy_life_expectancy']))
 
 plotly.offline.plot(fig, filename='sun')
-
-
-
-
-#--------------------------------SouthEastAsia-----------------------------------------------
-
-
-SouthEast_Asia=df[df.Regional_indicator=='Purples']
-print(SouthEast_Asia)
-
-
-
-
-#--------------------------------CentralEast-----------------------------------------------
-
-Central_and_Eastern_Europe=df[df.Regional_indicator=='Central_and_Eastern_Europe']
-print(Central_and_Eastern_Europe)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
