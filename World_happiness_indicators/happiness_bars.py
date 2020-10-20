e67fdfac4728abee323717160a433f503fa4979f
simofleanta <simo.fleanta@gmail.com>
2020-10-20 17:28:56 +0200

    s

--- World_happiness_indicators/happiness_bars.py
+++ World_happiness_indicators/happiness_bars.py
@@ -20,11 +20,27 @@ print(WestEu)
 
 h=WestEu.groupby(['Country_name'])['Perceptions_of_corruption'].mean()
 hap=pd.DataFrame(data=h)
-happy=hap.sort_values(by='Perceptions_of_corruption',ascending=False,axis=0)
+happy_corr=hap.sort_values(by='Perceptions_of_corruption',ascending=False,axis=0)
+print(happy_corr)
+
+fig = px.bar(happy_corr, x="Perceptions_of_corruption", y=happy_corr.index, color='Perceptions_of_corruption',color_continuous_scale='Teal',title="Perception of corruption in West EU")
+plotly.offline.plot(fig, filename='happy')
+
+
+h=WestEu.groupby(['Country_name'])['Generosity'].mean()
+hap=pd.DataFrame(data=h)
+happy=hap.sort_values(by='Generosity',ascending=False,axis=0)
 print(happy)
 
-fig = px.bar(happy, x="Perceptions_of_corruption", y=happy.index, color='Perceptions_of_corruption',color_continuous_scale='Teal',title="Perception of corruption in Western EU")
+fig = px.bar(happy, x="Generosity", y=happy.index, color='Generosity',color_continuous_scale='Teal',title="Generosity in Western EU")
 plotly.offline.plot(fig, filename='happy')
+
+
+
+
+
+
+
 
 #------------------------------------Central and Eastern EU------------------------
 
@@ -33,9 +49,9 @@ Central_and_Eastern_Europe=df[df.Regional_indicator=='Central_and_Eastern_Europe
 h=Central_and_Eastern_Europe.groupby(['Country_name'])['Perceptions_of_corruption'].mean()
 hap=pd.DataFrame(data=h)
 happyCE=hap.sort_values(by='Perceptions_of_corruption',ascending=False,axis=0)
-print(happyCE)
+
 
 fig = px.bar(happyCE, x="Perceptions_of_corruption", y=happyCE.index, color='Perceptions_of_corruption',color_continuous_scale='Teal',title="Perception of corruption in Central and Eastern EU")
-plotly.offline.plot(fig, filename='happy')
+"""plotly.offline.plot(fig, filename='happy')"""
 
 
