a47adb45dbd2b1df1081008ffb076f7ff0b8a832
simofleanta <simo.fleanta@gmail.com>
2020-10-21 19:41:17 +0200

    s

--- music/Spotify50.py
+++ music/Spotify50.py
@@ -40,6 +40,13 @@ genres_lodness=s.sort_values(by='Loudness_DB',ascending=False,axis=0)
 fig = px.bar(genres_lodness, x="Loudness_DB", y=genres_lodness.index, color='Loudness_DB',color_continuous_scale='Blues',title="Mean loudness on spotify genres")
 #plotly.offline.plot(fig, filename='bike')
 
+fig = px.bar(df, x="Artist_Name", y=["Beat_Per_Minute","Loudness_DB"],barmode='stack', color='Loudness_DB',color_continuous_scale='Blues',title="BPM and Lodness based on artists")
+#plotly.offline.plot(fig, filename='hap')
+
+fig = px.bar(df, x="Track_Name", y=["Beat_Per_Minute","Length","Acousticness"],barmode='stack', color='Popularity',color_continuous_scale='Blues',title="BPM and Acousticness based on artists")
+#plotly.offline.plot(fig, filename='hap')
+
+
 #----------------------------------------------------------------------------------------
 Loudness_DB=df['Loudness_DB']
 Energy=df['Energy']
@@ -48,6 +55,7 @@ Genre=df['Genre']
 Beat_Per_Minute=df['Beat_Per_Minute']
 Popularity=df['Popularity']
 Speechiness=df['Speechiness']
+Track_Name=df['Track_Name']
 
 fig0 = go.Figure(data=go.Heatmap(
                    z=Beat_Per_Minute,
@@ -75,7 +83,7 @@ fig.update_layout(
     title='spotify',
     xaxis_nticks=40)
 
-plotly.offline.plot(fig0, filename='bike')
+#plotly.offline.plot(fig0, filename='bike')
 
 fig0 = go.Figure(data=go.Heatmap(
                    z=Loudness_DB,
@@ -90,6 +98,23 @@ fig.update_layout(
     xaxis_nticks=40)
 
 #plotly.offline.plot(fig0, filename='bike')
+
+fig0 = go.Figure(data=go.Heatmap(
+                   z=Loudness_DB,
+                   x=Beat_Per_Minute,
+                   y=Track_Name,
+                   colorscale='Blues',
+                   ))
+
+fig.update_layout(    
+    title='Correlation between Loudness and BPM by track name',
+    xaxis_nticks=40)
+
+plotly.offline.plot(fig0, filename='bike')
+
+
+
+
 
 #treemaps
 
@@ -106,7 +131,7 @@ fig.update_layout(
     title_font_size=25,
     title_font_family='Arial'
 )
-plotly.offline.plot(fig, filename='bike')
+#plotly.offline.plot(fig, filename='bike')
 
     
     
