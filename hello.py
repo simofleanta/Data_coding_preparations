e2b0c983255427f547e02434a2b795a6beac317c
simofleanta <simo.fleanta@gmail.com>
2020-10-22 13:17:48 +0200

    s

--- hello.py
+++ hello.py
@@ -26,5 +26,37 @@ vc=df['party'].value_counts()
 #de=df.rename(columns={'x.Name':'y_Name'},inplace=True)
 #df.head()
 
+
+#grouping stuff
+
+#grouping by stuff
+
+e=df.groupby('party')['votes_second_vote'].mean()
+print(e)
+
 d=df.groupby(['party'])['votes_first_vote'].sum().sort_values(ascending =False)[:5]
-print(d)
+
+#reference columns for treemaps
+area_id=df['area_id']
+area_name=df['area_name']
+votes_first_vote=df['votes_first_vote']
+state=df['state']
+party=df['party']
+votes_second_vote=df['votes_second_vote']
+
+#barcharts
+
+fig = go.Figure(data=go.Heatmap(
+                   z=votes_second_vote,
+                   x=state,
+                   y=party,
+                   colorscale='Blues'))
+
+fig.update_layout(
+    
+    title='Votes in German elections 2017 heatmap',
+    xaxis_nticks=40)
+
+#plotly.offline.plot(fig, filename='votes')
+
+
