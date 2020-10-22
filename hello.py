7609d14935e4236fd5f3e35e41c27c13873de727
simofleanta <simo.fleanta@gmail.com>
2020-10-22 14:08:11 +0200

    add code

--- hello.py
+++ hello.py
@@ -60,3 +60,39 @@ fig.update_layout(
 #plotly.offline.plot(fig, filename='votes')
 
 
+#extract party
+
+
+CDU=df[df.party=='Christlich.Demokratische.Union.Deutschlands']
+print(CDU)
+
+area_id=CDU['area_id']
+area_name=CDU['area_name']
+votes_first_vote=CDU['votes_first_vote']
+State=CDU['state']
+Party=CDU['party']
+voteSecondVote=CDU['votes_second_vote']
+
+v=CDU.groupby(['state'])['votes_second_vote'].mean()
+el=pd.DataFrame(data=v)
+Votes=el.sort_values(by='votes_second_vote',ascending=False,axis=0)
+print(Votes)
+
+fig = px.bar(Votes, x="votes_second_vote", y=Votes.index, color='votes_second_vote',color_continuous_scale='Blues',title="CDU 2nd votes")
+#plotly.offline.plot(fig, filename='bike')
+
+
+v=CDU.groupby(['state'])['votes_first_vote'].mean()
+el=pd.DataFrame(data=v)
+cduVotes=el.sort_values(by='votes_first_vote',ascending=False,axis=0)
+print(cduVotes)
+
+ig = px.bar(cduVotes, x="votes_first_vote", y=cduVotes.index, color='votes_first_vote',color_continuous_scale='Blues',title="CDU 1st votes")
+#plotly.offline.plot(fig, filename='bike')
+
+
+fig = px.bar(CDU, x="state", y=["votes_second_vote","votes_first_vote"],barmode='stack', color='votes_first_vote',color_continuous_scale='Blues',
+title="CDU 1st and 2nd votes performance across states in Germany")
+#plotly.offline.plot(fig, filename='hap')
+
+
