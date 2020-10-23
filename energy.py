6a08b9e54e1d887cafa6fcb9e2a378998bbf13d3
simofleanta <simo.fleanta@gmail.com>
2020-10-23 11:19:08 +0200

    add grup by check the datetime looks fine Coment on normalize

--- energy.py
+++ energy.py
@@ -14,3 +14,21 @@ g=pd.read_csv('AEP_hourly.csv')
 df=DataFrame(g)
 print(df.columns)
 print(df.head(10))
+
+
+#no missing_v
+missing_v=df.isnull().sum()
+print(missing_v)
+vc=df['Datetime'].value_counts()
+
+#group AEP_MW by datetime :)
+e=df.groupby('Datetime')['AEP_MW'].mean()
+print(e.head(5))
+
+df.plot(figsize=(16,4),legend=True)
+
+plt.title('AEP hourly power consumption data - BEFORE NORMALIZATION')
+
+plt.show()
+
+#NORMALIZE DATA BEFORE ANALYSIS 
