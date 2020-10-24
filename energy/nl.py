f1d061485878d671fab9e5fef2e2ec5f9eaee62a
simofleanta <simo.fleanta@gmail.com>
2020-10-24 08:48:26 +0200

    save

--- energy/nl.py
+++ energy/nl.py
@@ -14,25 +14,25 @@ import plotly
 g=pd.read_csv('enexis_electricity_01012020.csv')
 #print(g.columns)
 df=DataFrame(g)
-print(df.columns)
-print(df.head(10))
+#print(df.columns)
+#print(df.head(10))
 
 
-"""def normalize_data(df):
+
+
+def normalize_data(df):
     scaler = sklearn.preprocessing.MinMaxScaler()
-    df['AEP_MW']=scaler.fit_transform(df['AEP_MW'].values.reshape(-1,1))
+    df['annual_consume']=scaler.fit_transform(df['annual_consume'].values.reshape(-1,1))
     return df
 
 df_norm = normalize_data(df)
 
 shape=df_norm.shape
 #print(shape)
-#print(df_norm.head(3))"""
+#print(df_norm.head(3))
 
 
 
-
-#import plotly.express as px
-#df = px.data.tips()
-#fig = px.density_heatmap(df_norm, x="Datetime", y="AEP_MW", nbinsx=20, nbinsy=20, color_continuous_scale="YlOrRd",title='2d histograms on hourly energy consumption 2004-2018 :)')
-#plotly.offline.plot(fig, filename='bikes on a day')
+df = px.data.tips()
+fig = px.density_heatmap(df_norm, x="city", y="annual_consume", nbinsx=20, nbinsy=20, color_continuous_scale="YlOrRd",title='2d histograms on hourly energy consumption 2004-2018 :)')
+plotly.offline.plot(fig, filename='bikes on a day')
