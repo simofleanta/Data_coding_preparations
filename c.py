6d8cb3de3fa000278ad0466a9a98951d4f33c3e6
simofleanta <simo.fleanta@gmail.com>
2020-11-02 17:22:22 +0100

    save

--- c.py
+++ c.py
@@ -12,37 +12,37 @@ import statistics
 
 #open file
 c=pd.read_csv('day.csv')
-print(c.columns)
+#print(c.columns)
 df=DataFrame(c.head(3))
-print(df.head(10))
+#print(df.head(10))
 
 #check missing_v
 missing_v=df.isnull().sum()
 vc=df['cnt'].value_counts()
-print(vc)
+#print(vc)
 
 #check dtypes before change
 types=df.dtypes
-print(types)
+#print(types)
 
 #change area in cat
 mnth_cat=df.mnth=pd.Categorical(df['mnth'], ordered=True)
-print(mnth_cat)
+#print(mnth_cat)
 weekday_cat=df.weekday=pd.Categorical(df['weekday'], ordered=True)
-print(weekday_cat)
+#print(weekday_cat)
 #check dtypes after change
 types=df.dtypes
-print(types)
+#print(types)
 
 
 m=df.cnt.mean()
-print(m)
+#print(m)
 n=df.groupby('weekday').cnt.mean()
 print(n)
 P=df.groupby('mnth').cnt.mean()
-print(P)
+#print(P)
 P=df.groupby('season').cnt.mean()
-print(P)
+#print(P)
 
 
 
