4e4c3f904798cd888b24ec6384ac55472ede9e69
simofleanta <simo.fleanta@gmail.com>
2020-11-02 19:14:15 +0100

    s

--- c.py
+++ c.py
@@ -15,10 +15,57 @@ import stats
 c=pd.read_csv('cars_csv.csv')
 #print(c.columns)
 df=DataFrame(c.head(3))
-print(df.head(10))
+#print(df.head(10))
 
-df.describe()
-df.sum()
+#df.describe()
+#df.sum()
+#df.mean()
+#df.max()
+
+
+#Min/max on separate columns
+mpg=df.mpg
+s=mpg.max()
+#print(s)
+
+wt=df.wt
+f=wt.min()
+#print(f)
+
+"""summary stats"""
+#df stdev
+s=df.std()
+#print(s)
+
+mean=df.mean()
+#print(mean)
+
+""" Agreggate"""
+d=df.agg(['mean','max','min','var','count'])
+#print(d)
+
+"""group by 2 columns """
+
+g=df.groupby('cyl').mean()
+#print(g)
+x=df.groupby(['cars_names'])[['cyl']]
+print(x.mean())
+
+
+#
+x=df.groupby(['cars_names'])[['carb']]
+print(x.mean())
+
+
+
+
+
+
+
+
+
+
+
 
 
 
