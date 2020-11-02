1084d9a63cd99555354c041180cc6b6a31559989
simofleanta <simo.fleanta@gmail.com>
2020-11-02 17:05:30 +0100

    changesd the dataset

--- c.py
+++ c.py
@@ -11,14 +11,14 @@ import plotly
 import statistics
 
 #open file
-c=pd.read_csv('c.csv')
+c=pd.read_csv('day.csv')
 print(c.columns)
 df=DataFrame(c.head(3))
-print(df.head(50))
+print(df.head(10))
 
 #check missing_v
 missing_v=df.isnull().sum()
-vc=df['TIME'].value_counts()
+vc=df['cnt'].value_counts()
 print(vc)
 
 #check dtypes before change
@@ -26,17 +26,28 @@ types=df.dtypes
 print(types)
 
 #change area in cat
-area_cat=df.AREA=pd.Categorical(df['AREA'], ordered=True)
-print(area_cat)
-time_cat=df.TIME=pd.Categorical(df['TIME'], ordered=True)
-print(time_cat)
+mnth_cat=df.mnth=pd.Categorical(df['mnth'], ordered=True)
+print(mnth_cat)
+weekday_cat=df.weekday=pd.Categorical(df['weekday'], ordered=True)
+print(weekday_cat)
 #check dtypes after change
 types=df.dtypes
 print(types)
 
 
-pasta=df['AREA'].mean()
-print(pasta)
+m=df.cnt.mean()
+print(m)
+n=df.groupby('weekday').cnt.mean()
+print(n)
+P=df.groupby('mnth').cnt.mean()
+print(P)
+P=df.groupby('season').cnt.mean()
+print(P)
+
+
+
+
+
 
 
 
