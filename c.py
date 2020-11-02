d63500b79eae0d19e72c2bb21b126d71c8038f2a
simofleanta <simo.fleanta@gmail.com>
2020-11-02 19:49:17 +0100

    s

---
+++ c.py
@@ -0,0 +1,53 @@
+
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import plotly
+import statistics
+import stats
+
+
+c=pd.read_csv('c.csv')
+#print(c.columns)
+df=DataFrame(c.head(20))
+print(df.head(20))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
