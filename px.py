201beb4eeddcbe6f0416997afa7b0bd844ae6254
simofleanta <simo.fleanta@gmail.com>
2020-11-08 11:12:04 +0100

    add groupings by

--- px.py
+++ px.py
@@ -9,8 +9,15 @@ import plotly
 import statistics
 import stats
 
-c=pd.read_csv('c.csv')
+c=pd.read_csv('heatmap_px.csv')
 #print(c.columns)
 df=DataFrame(c.head(100))
-print(df.head(100))
+#print(df.head(100))
 
+x=df.groupby(['yr'])[['px']]
+print(x.mean())
+
+y=df.groupby(['company'])[['px']].mean()
+print(y)
+
+z=
