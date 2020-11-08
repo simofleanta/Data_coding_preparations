a9e2ca1957f0c1f92a18113892d423eca896d757
simofleanta <simo.fleanta@gmail.com>
2020-11-08 13:25:53 +0100

    add plt

--- px.py
+++ px.py
@@ -18,21 +18,19 @@ print(df.head(100))
 correlation=df.corr(method='pearson')
 #print(correlation)
 
+"""Groupings and agg"""
+
 x=df.groupby(['yr'])[['px']]
 #print(x.mean())
 
 y=df.groupby(['company'])[['px']].mean()
 #print(y)
 
-El=df[df.company=='EL']
-#print(El)
-
-electrica=El.corr(method='pearson')
-print(electrica)
-
-
 El=df.groupby(['px']).mean()
 #print(El)
+
+y=df.groupby(['company'])[['px']].mean()
+#print(y)
 
 #Aggregate
 operations=['mean', 'std','sum','min','max']
@@ -40,16 +38,37 @@ sd=df.groupby(['company','yr'], as_index=False)[['px']].agg(operations)
 #print(sd.reset_index())
 
 
-y=df.groupby(['company'])[['px']].mean()
-#print(y)
+"""correlations Pearson for companies"""
+
+El=df[df.company=='EL']
+brd=df[df.company=='BRD']
+snp=df[df.company=='SNP']
+sng=df[df.company=='SNG']
+tlv=df[df.company=='TLV']
+year=df['yr']
+company=df['company']
+
+ELec=El.corr(method='pearson')
+brd_generale=brd.corr(method='pearson')
+Snp=snp.corr(method='pearson')
+Sng=sng.corr(method='pearson')
+Tlv=tlv.corr(method='pearson')
+#print(ELec)
+#print(brd_generale)
+#print(Snp)
+#print(Sng)
+#print(Tlv)
+sns.pairplot(ELec)
+plt.show()
 
 
-#tables 
-import plotly.figure_factory as ff
 
-df=sd
 
-c=df.corr(method='pearson')
+
+
+
+
+
 
 
 
