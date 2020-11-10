9eae6492c73b65142c8f45c1dc277b9cf665e0be
simofleanta <simo.fleanta@gmail.com>
2020-11-10 13:18:29 +0100

    save

--- World_happiness_indicators/dojo.py
+++ World_happiness_indicators/dojo.py
@@ -4,6 +4,7 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import plotly
 import statistics
@@ -28,15 +29,18 @@ b=df.dtypes
 c=df.select_dtypes(object)
 #print(c)
 
-from sklearn.preprocessing import LabelEncoder
+# there are 10 object type columns in data we will convert this to numberical using LabelEncoder
 encoder=LabelEncoder()
-data['CarName']=encoder.fit_transform(df['CarName'])
-data['fueltype']=encoder.fit_transform(df['fueltype'])
-data['aspiration']=encoder.fit_transform(df['aspiration'])
-data['doornumber']=encoder.fit_transform(df['doornumber'])
-data['carbody']=encoder.fit_transform(df['carbody'])
-data['drivewheel']=encoder.fit_transform(df['drivewheel'])
-data['enginelocation']=encoder.fit_transform(df['enginelocation'])
-data['enginetype']=encoder.fit_transform(df['enginetype'])
-data['cylindernumber']=encoder.fit_transform(df['cylindernumber'])
-data['fuelsystem']=encoder.fit_transform(df['fuelsystem'])
+df['CarName']=encoder.fit_transform(df['CarName'])
+df['fueltype']=encoder.fit_transform(df['fueltype'])
+df['aspiration']=encoder.fit_transform(df['aspiration'])
+df['doornumber']=encoder.fit_transform(df['doornumber'])
+df['carbody']=encoder.fit_transform(df['carbody'])
+df['drivewheel']=encoder.fit_transform(df['drivewheel'])
+df['enginelocation']=encoder.fit_transform(df['enginelocation'])
+df['enginetype']=encoder.fit_transform(df['enginetype'])
+df['cylindernumber']=encoder.fit_transform(df['cylindernumber'])
+df['fuelsystem']=encoder.fit_transform(df['fuelsystem'])
+
+c=df.dtypes
+#print(c)
