850f978f184baa3485a2e8f605f497fe03732e9f
simofleanta <simo.fleanta@gmail.com>
2020-11-10 18:37:32 +0100

    extracted month, grouped by past mnths

--- business_plan.py
+++ business_plan.py
@@ -18,8 +18,8 @@ from sklearn.metrics import r2_score
 
 c=pd.read_csv('bike_business_plan.csv')
 #print(c.columns)
-df=DataFrame(c.head(100))
-#print(df.head(100))
+df=DataFrame(c.head(700))
+#print(df.head(700))
 
 c=df.select_dtypes(object)
 #print(c)
@@ -43,6 +43,8 @@ print(a.reset_index())
 
 df['Number_Bikes'].value_counts().sort_values(ascending=False).head(10)
 
+#business best months
+
 fig, ax=plt.subplots(figsize=(6,4))
 df['Month'].value_counts().sort_values(ascending=False).head(10).plot(kind='bar')
 plt.ylabel('Number_Bikes')
@@ -50,6 +52,7 @@ plt.xlabel('Month')
 ax.get_yaxis().get_major_formatter().set_scientific(False)
 plt.title('Best months')
 
+#business during week
 
 fig, ax=plt.subplots(figsize=(6,4))
 sns.set_style('darkgrid')
@@ -57,6 +60,37 @@ df.groupby('weekday')['Price'].count().sort_values().plot(kind='bar')
 plt.ylabel('Price')
 ax.get_yaxis().get_major_formatter().set_scientific(False)
 plt.title('Business during the week')
+
+#that when is the bike business doing the tbest  during the day. 
+
+fig, ax=plt.subplots(figsize=(6,4))
+sns.set_style('darkgrid')
+df.groupby('Day_Time')['Number_Bikes'].count().sort_values().plot(kind='bar')
+plt.ylabel('Number_Bikes')
+ax.get_yaxis().get_major_formatter().set_scientific(False)
+plt.title('Business during the day')
+
+sortbyday=df.groupby('Day_Time')['Number_Bikes'].count().sort_values(ascending=False)
+print(sortbyday)
+
+#business in the past months
+
+df.groupby('Month')['Number_Bikes'].count().plot(kind='bar')
+plt.ylabel('Number_Bikess')
+plt.title('Bikes during the past months')
+
+#extract month situation
+Okt=df.loc[df['Month']=='Okt'].nunique()
+print(Okt)
+
+#pivots
+
+
+
+
+
+
+
 
 
 
