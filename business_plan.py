6fcd2d6fa3fb329ab5ccb8eb5cbde415cd6b1c0a
simofleanta <simo.fleanta@gmail.com>
2020-11-10 18:11:21 +0100

    save the improvement

---
+++ business_plan.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+
+
+c=pd.read_csv('bike_business_plan.csv')
+#print(c.columns)
+df=DataFrame(c.head(100))
+#print(df.head(100))
+
+c=df.select_dtypes(object)
+#print(c)
+
+#trasnform in numerical
+encoder=LabelEncoder()
+df['Number_Bikes']=encoder.fit_transform(df['Number_Bikes'])
+
+c=df.dtypes
+#print(c)
+
+#groupings
+x=df.groupby(['Season'])[['Number_Bikes']]
+print(x.mean())
