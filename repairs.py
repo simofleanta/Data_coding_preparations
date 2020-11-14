de19e2f5f2cd6c40f570c2e2e81bcd18bb2347f3
simofleanta <simo.fleanta@gmail.com>
2020-11-14 21:52:31 +0100

    save

--- repairs.py
+++ repairs.py
@@ -19,34 +19,12 @@ from datetime import datetime
 
 
 
-repairs=pd.read_csv('device_repairs.csv')
+repairs=pd.read_csv('repairs.csv')
 print(repairs.columns)
 df=DataFrame(repairs)
 print(df)
 
 
-x_shape=df.shape
-print(x_shape)
-x_describe=df.describe()
-print(x_describe)
-
-#check repaircost sum by timestamp and device_id
-
-operations=['sum','min','max']
-a=df.groupby(['timestamp_utc', 'device_id'], as_index=False)[['repair_cost']].agg(operations)
-print(a.reset_index())
-
-
-#extract day 
-day2=df[df.timestamp_utc=='2/2/2020']
-print(day2)
-
-operations=['sum','min','max']
-a=day2.groupby(['timestamp_utc', 'device_id'], as_index=False)[['repair_cost']].agg(operations)
-print(a.reset_index())
-
-x=df.groupby(['repair_cost', 'timestamp_utc'])
-print(x.sum())
 
 #use py to analyse whatever and make graphs 
 #analyze whatever 
