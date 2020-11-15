732dd4c4692a9a684a9820eece024120391677a9
simofleanta <simo.fleanta@gmail.com>
2020-11-15 11:27:26 +0100

    save

--- deffects.py
+++ deffects.py
@@ -41,12 +41,18 @@ numerical=df['date']=encoder.fit_transform(df['date'])
 
 #Date mean 50 you get 200 screenbroken defects compared to software defects, 7025 in date mean 58 
 
-operations=['mean','sum','min','max']
-defect=df.groupby(['defect_category'], as_index=False)[['date']].agg(operations)
+operations=['sum']
+defect=df.groupby(['defect_category','timestamp_utc'], as_index=False)[['date']].agg(operations)
 print(defect.reset_index())
 
-defect_date_num=df.groupby(['date','timestamp_utc'])
-print(defect_date_num.sum())
+#pivot
+
+pivot=df.pivot_table(index='defect_category',columns='timestamp_utc', aggfunc={'date':'sum'}).fillna(0)
+pivot['Max']=pivot.idxmax(axis=1)
+print(pivot)
+
+
+
 
 
 
