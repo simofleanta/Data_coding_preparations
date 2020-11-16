ba3e43fbbab6754c7f4e0bcc586aff7f122eec00
simofleanta <simo.fleanta@gmail.com>
2020-11-16 14:24:29 +0100

    save sql

---
+++ sqlscript.sql
@@ -0,0 +1,106 @@
+REM   Script: Session 01
+REM   mno
+
+select*from SH.Sales;
+
+select CUST_ID from SH.Sales;
+
+select CUST_ID from SH.Sales;
+
+select *from SH.Sales;
+
+select CUST_ID, AMOUNT_ID from SH.Sales;
+
+select CUST_ID AND AMOUNT_ID from SH.Sales;
+
+select AMOUNT_ID from SH.Sales;
+
+select * from SH.Sales;
+
+select * AMOUNT_SOLD, CUST_ID from SH.Sales;
+
+select * AMOUNT_SOLD from SH.Sales;
+
+select  AMOUNT_SOLD, CUST_ID from SH.Sales;
+
+select  AMOUNT_SOLD, CUST_ID from SH.Sales 
+select top n * from SH.Sales ;
+
+select  AMOUNT_SOLD, CUST_ID from SH.Sales 
+select TOP N * from SH.Sales ;
+
+select  AMOUNT_SOLD, CUST_ID from SH.Sales 
+;
+
+select  AMOUNT_SOLD, CUST_ID from SH.Sales 
+;
+
+select  AMOUNT_SOLD, CUST_ID from SH.Sales 
+SELECT TOP AMOUNT_SOLD * FROM SH.Sales;
+
+SELECT TOP AMOUNT_SOLD * FROM SH.Sales;
+
+select  AMOUNT_SOLD, CUST_ID from SH.Sales 
+;
+
+select  AMOUNT_SOLD, CUST_ID from SH.Sales 
+ORDER AMOUNT_SOLD DESC;
+
+SELECT * FROM hr.employees;
+
+SELECT * FROM hr.employees 
+select top N* from employees order by salary desc;
+
+SELECT * FROM hr.employees 
+select top N* from employees ;
+
+SELECT * FROM hr.employees 
+select top N* from employees  
+order by salary desc;
+
+SELECT * FROM hr.employees 
+select emp_name AS UPPER;
+
+SELECT * FROM hr.employees 
+SELECT DISTINCT column1;
+
+SELECT * FROM hr.employees 
+SELECT DISTINCT departments, employees;
+
+SELECT DISTINCT departments, employees, ... 
+FROM hr.Employees;
+
+select * from hr.employees 
+;
+
+select DISTINCT FIRST_NAME, SALARY 
+from hr.employees 
+ 
+;
+
+select DISTINCT FIRST_NAME, SALARY 
+from hr.employees 
+ 
+SELECT Last_Name from hr.employees 
+;
+
+select * from hr.employee 
+ 
+ 
+;
+
+select* from hr.employee 
+;
+
+select * from hr.employees 
+;
+
+select * from hr.employees 
+select DISTINCT FIRST_NAME, SALARY 
+from hr.employees 
+select HIRE_DATE FROM hr.employees 
+;
+
+select HIRE_DATE FROM hr.employees 
+;
+
