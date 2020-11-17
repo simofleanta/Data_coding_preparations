60db3e7f7f7a548a498a3f03152cde06305222b9
simofleanta <simo.fleanta@gmail.com>
2020-11-17 16:48:26 +0100

    add script

---
+++ SQL/sql2.sql
@@ -0,0 +1,18 @@
+REM   Script: Alias
+REM   K
+
+select employee_id AS ID from hr.employees;
+
+select employee_id AS ID from hr.employees;
+
+select JOB_ID as jobs from hr.emplyees;
+
+select employee_id AS ID from hr.employees;
+
+select JOB_ID AS JOBS from hr.EMPLOYEES;
+
+select employee_id AS ID from hr.employees;
+
+select JOB_ID AS JOBS from hr.EMPLOYEE 
+LIMIT 3;
+
