6bfcde5d76e337a452a027fd34ee1807830247a6
simofleanta <simo.fleanta@gmail.com>
2020-11-17 17:04:10 +0100

    add

---
+++ SQL/update_set.sql
@@ -0,0 +1,548 @@
+REM   Script: Set update to modify
+REM   Ok
+
+select * from hr.employees;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select email,phone_number 
+from hr.employees  
+where job_id='fi_account';
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT', SALARY<10000;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT', and where SALARY<10000;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT', where SALARY<10000;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT' where SALARY<10000;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT' where SALARY<17000;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT', SALARY<17000;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME from hr.employees LIKE 'A' 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A' 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+select FIRST_NAME,SALARY FROM hr.employees WHERE SALARY like '9%' 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+select FIRST_NAME,SALARY FROM hr.employees WHERE SALARY like 9 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+select FIRST_NAME,SALARY FROM hr.employees WHERE first_name like'b%' 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+select FIRST_NAME,SALARY FROM hr.employees WHERE FIRST_NAME LIKE 'B%' 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+select FIRST_NAME,SALARY FROM hr.employees WHERE FIRST_NAME LIKE 'B%';
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+ 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'B%' 
+ 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+ 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+SELECT FIRST_NAME, SALARY FROM HR.EMPLOYEES WHERE FIRST_NAME LIKE 'B%' 
+ 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+SELECT FIRST_NAME, SALARY FROM hr.employees WHERE FIRST_NAME LIKE 'B%' 
+ 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+SELECT FIRST_NAME, job_id FROM hr.employees WHERE FIRST_NAME LIKE 'B%' 
+ 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+SELECT FIRST_NAME,JOB_ID, FROM hr.employees  
+where salary <9000 and job_id='FI_ACCOUNT'and FIRST_NAME LIKE 'B%' 
+ 
+ 
+ 
+;
+
+select * from hr.employees;
+
+select count(distinct job_id) from hr.employees;
+
+select EMAIL,PHONE_NUMBER from hr.employees  
+where JOB_ID='FI_ACCOUNT';
+
+select FIRST_NAME, SALARY from hr.employees WHERE FIRST_NAME LIKE 'A%' 
+SELECT FIRST_NAME,JOB_ID, FROM hr.employees  
+where salary <9000 and job_id='FI_ACCOUNT';
+
+select FIRST_Name, salary from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where salary <9000;
+
+select FIRST_Name, salary from hr.employees 
+where salary <9000 asc;
+
+select FIRST_Name, salary from hr.employees 
+where salary <9000;
+
+select FIRST_Name, salary from hr.employees 
+where salary <9000 and job_id='fi_account';
+
+select FIRST_Name, salary from hr.employees 
+where salary <17000 and job_id='fi_account';
+
+select*from hr.employees 
+select FIRST_Name, salary from hr.employees 
+where salary <17000 and job_id='fi_account';
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where salary <17000 and job_id='fi_account';
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where salary <17000 and job_id='it_prog';
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where First_name like'a' and job_id='it_prog';
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where First_name like'a% and job_id='it_prog';
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where First_name like'a%' and job_id='it_prog';
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where Last_name like'a%' and job_id='it_prog';
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where Last_name like'A%' and job_id='it_prog';
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where LAST_NAME like'A%' and job_id='it_prog';
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where job_id='it_prog';
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where job_id='it_prog';
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+;
+
+select*from hr.employees;
+
+select FIRST_Name, salary from hr.employees;
+
+select FIRST_Name, salary from hr.employees;
+
+select FIRST_Name, salary from hr.employees 
+where job_id='it_prog' 
+ 
+;
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG' 
+ 
+;
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<=9000;
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<9000;
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<9000 and FIRST_NAME LIKE'B' 
+ 
+;
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<9000 and FIRST_NAME LIKE'B%' 
+ 
+;
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<9000 and FIRST_NAME LIKE'Diana' 
+ 
+;
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<9000 and FIRST_NAME LIKE'Diana' 
+where JOB_ID='FI_ACCOUNT' and LAST_NAME LIKE'A%' 
+;
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<9000 and FIRST_NAME LIKE'Diana', 
+where JOB_ID='FI_ACCOUNT' and LAST_NAME LIKE'A%' 
+;
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<9000 and FIRST_NAME LIKE'Diana', 
+where JOB_ID='FI_ACCOUNT' and LAST_NAME LIKE'A%';
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<9000 and FIRST_NAME LIKE'Diana' 
+where JOB_ID='FI_ACCOUNT' and LAST_NAME LIKE'A%';
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<9000 and FIRST_NAME LIKE'Diana' 
+and JOB_ID='FI_ACCOUNT' and LAST_NAME LIKE'A%';
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<9000 and FIRST_NAME LIKE'Diana' 
+and JOB_ID='FI_ACCOUNT' and LAST_NAME LIKE'A%' 
+;
+
+select FIRST_Name, salary from hr.employees 
+where JOB_ID='IT_PROG'and salary<9000 and FIRST_NAME LIKE'Diana' 
+;
+
+select * from hr.countries;
+
+select * from hr.countries 
+select*from hr.employees;
+
+select * from hr.countries 
+;
+
+select * from hr.employees 
+;
+
+select * from hr.employees 
+select count(distinct job_id) from hr.employees  
+where not job_id='FI_ACCOUNT';
+
+select * from hr.employees 
+select count(distinct job_id) 
+where not job_id='FI_ACCOUNT';
+
+select * from hr.employees 
+where count(distinct JOB_ID) not job_id='FI_ACCOUNT';
+
+select count(distinct job_id) from hr.employees 
+;
+
+select count(distinct job_id) from hr.employees 
+where not job_id='fi_aacount' 
+;
+
+select count(distinct job_id) from hr.employees 
+where not job_id='fi_account' 
+;
+
+select count(distinct job_id) from hr.employees 
+WHERE NOT JOB_ID='FI_ACCOUNT' 
+;
+
+select count(distinct job_id) from hr.employees 
+WHERE NOT JOB_ID='FI_ACCOUNT' and 'IT_PROG' 
+;
+
+select count(distinct job_id) from hr.employees 
+WHERE NOT JOB_ID='FI_ACCOUNT', 'IT_PROG' 
+;
+
+select count(distinct job_id) from hr.employees 
+WHERE NOT JOB_ID='FI_ACCOUNT'  
+;
+
+select count(distinct job_id) from hr.employees 
+WHERE NOT JOB_ID='FI_ACCOUNT' AND NOT 'IT_PROG' 
+;
+
+select count(distinct job_id) from hr.employees 
+WHERE NOT JOB_ID='FI_ACCOUNT' AND NOT JOB_ID='IT_PROG' 
+;
+
+select count(distinct job_id), salary from hr.employees 
+WHERE NOT JOB_ID='FI_ACCOUNT' AND NOT JOB_ID='IT_PROG' 
+;
+
+select count(distinct job_id)from hr.employees 
+WHERE NOT JOB_ID='FI_ACCOUNT' AND NOT JOB_ID='IT_PROG'  
+ 
+ 
+;
+
+select count(distinct job_id)from hr.employees 
+WHERE NOT JOB_ID='FI_ACCOUNT' AND NOT JOB_ID='IT_PROG'  
+ 
+select max(salary) from hr.employees 
+ 
+;
+
+select count(distinct job_id)from hr.employees 
+WHERE NOT JOB_ID='FI_ACCOUNT' AND NOT JOB_ID='IT_PROG'  
+ 
+select * from hr.employees 
+select max(salary) from hr.employees where salary<(max(salary) from hr.employees);
+
+select *from hr.employees;
+
+select *from hr.employees 
+select max(salary) from hr.employees where salary <(select max(salary) from hr.employees);
+
+select *from hr.employees 
+select max(salary) from hr.employees where salary < 
+(select max(salary) from hr.employees);
+
+select *from hr.employees 
+select max(salary) from hr.employees ;
+
+select *from hr.employees 
+select max(salary) from hr.employees  
+where salary <;
+
+select *from hr.employees 
+select max(salary) from hr.employees  
+where salary < 24000;
+
+select *from hr.employees 
+select max(salary) from hr.employees  
+where salary < (select max(SALARY) from hr.emplyees);
+
+select *from hr.employees 
+where salary < (select max(SALARY) from hr.emplyees);
+
+select *from hr.employees 
+where SALARY < 24000(select max(SALARY) from hr.emplyees);
+
+select * from hr.employees;
+
+select * from hr.employees 
+update hr.employees 
+set JOB_ID='PythonDev'  
+where EMPLOYEE_ID=100;
+
+select * from hr.employees 
+update hr.employees 
+set JOB_ID='PythonDev' where EMPLOYEE_ID=100;
+
+update hr.employees 
+set JOB_ID='PythonDev' where EMPLOYEE_ID=100;
+
+update hr.employees 
+set JOB_ID='PythonDev';
+
+select FIRST_NAME, JOB_ID FROM HR.EMPLOYEES;
+
+select FIRST_NAME, JOB_ID FROM HR.EMPLOYEES 
+update HR.EMPLOYEES  
+SET FIRST_NAME='Steve' 
+where JOB_ID='SA_REP';
+
+select FIRST_NAME, JOB_ID FROM HR.EMPLOYEES 
+update HR.EMPLOYEES  
+SET FIRST_NAME='Steve' 
+where JOB_ID='SA_REP';
+
