2136a8b62bffc449f04509051b3c5278981f39c7
simofleanta <simo.fleanta@gmail.com>
2020-11-17 16:50:38 +0100

    save

---
+++ SQL/innerjoin.sql
@@ -0,0 +1,6 @@
+# inner join 
+
+select suppliers.SupplierID, suppliers.SupplierName,suppliers.City,products.ProductName,products.Price 
+from suppliers
+inner join products
+on suppliers.SupplierID=products.SupplierID;
