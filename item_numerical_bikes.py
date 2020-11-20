db1bd0c43a7e28381d7e987c70fb2082b45e880f
simofleanta <simo.fleanta@gmail.com>
2020-11-20 13:25:48 +0100

    add to business plan item numerical

---
+++ item_numerical_bikes.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+
+
+
+c=pd.read_csv('bike_business_plan.csv')
+#print(c.columns)
+df=DataFrame(c.head(700))
+print(df.head(700))
+
+#
+encoder=LabelEncoder()
+df['Sales']=encoder.fit_transform(df['Sales'])
+
+#
+
+sns.violinplot(x=df["Item"], y=df["Sales"], palette="Blues")
+plt.show()
+
+c=df.select_dtypes(object)
+#print(c)
+
+#trasnform in numerical
+encoder=LabelEncoder()
+df['Number_Bikes']=encoder.fit_transform(df['Number_Bikes'])
+
+c=df.dtypes
+#print(c)
+
+#groupings
+x=df.groupby(['Season'])[['Number_Bikes']]
+#print(x.mean())
+
+
+#Aggregate
+operations=['mean','sum','min','max']
+a=df.groupby(['Year','Month'], as_index=False)[['Number_Bikes']].agg(operations)
+print(a.reset_index())
+
+df['Number_Bikes'].value_counts().sort_values(ascending=False).head(10)
+
+sns.violinplot(x=df["Month"], y=df["Number_Bikes"], palette="Blues")
+plt.show()
+
+
+
+#when is the bike business doing the tbest  during the day. 
+
+fig, ax=plt.subplots(figsize=(6,4))
+sns.set_style('darkgrid')
+df.groupby('Day_Time')['Item'].count().sort_values().plot(kind='bar')
+plt.ylabel('Number_Bikes')
+ax.get_yaxis().get_major_formatter().set_scientific(False)
+plt.title('Business during the day')
+plt.show()
+
+
+sortbyday=df.groupby('Day_Time')['Item'].count().sort_values(ascending=False)
+
+#business in the past months
+
+df.groupby('Item')['Month'].count().plot(kind='bar')
+plt.ylabel('Number_Bikess')
+plt.title('Bikes number during the past months')
+plt.show()
+
+#extract month situation
+Okt=df.loc[df['Month']=='Okt'].nunique()
+
+
+#pivots. I should add the bike brand name so I can see which one is the pivot one
+pivot1=df.pivot_table(index='Season',columns='Item', aggfunc={'Number_Bikes':'count'}).fillna(0)
+pivot1['Max']=pivot1.idxmax(axis=1)
+print(pivot1)
+
+df.groupby('Month')['Sales'].sum().plot(kind='bar')
+plt.ylabel('Sales')
+plt.title('Bikes sales in the last months')
+plt.show()
+
+df.groupby('Year')['Sales'].sum().plot(kind='bar')
+plt.ylabel('Sales')
+plt.title('2019-2020 comparison')
+plt.show()
