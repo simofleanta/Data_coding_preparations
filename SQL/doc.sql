4bcb2d644dd622a48caad9a18b730628f5ba52cd
simofleanta <simo.fleanta@gmail.com>
2020-11-20 08:51:10 +0100

    s

--- SQL/doc.sql
+++ SQL/doc.sql
@@ -2,3 +2,9 @@
 -- update  increase 3% repair_cost 
 update repairs set repair_cost =repair_cost +(repair_cost * 5/100);
 
+--return device id starting with letter W
+select device_id from repairs  
+where first_name like 'W%';
+
+
+
