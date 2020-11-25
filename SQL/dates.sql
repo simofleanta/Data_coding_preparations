d1ec548810b32f2ec2d93f0383bea0733e6e6a80
simofleanta <simo.fleanta@gmail.com>
2020-11-25 19:21:15 +0100

    s

--- SQL/dates.sql
+++ SQL/dates.sql
@@ -11,17 +11,36 @@ SELECT First_Name, job_id,salary, Extract(DAY FROM hire_date) AS hire FROM hr.em
 where salary between 9000 and 17000;
 
 --last day of month 
+select LAST_DAY('07-JUN-02') from hr.employees;
 
-elect LAST_DAY('07-JUN-02') from hr.employees;
+--sys dual date
+Select SYSDATE, LAST_DAY('07-JUN-02') 
+"Last Day" from dual;
+
+-- how many months between the two dates 
+Select  MONTHS_BETWEEN ('30-JUN-02','17-AUG-02')
+"Months" from hr.employees
 
 
-
+--exatrct day and order it by smth(repair_phones other data)
 SELECT First_Name, Extract(DAY FROM hire_date) AS hire FROM hr.employees;
-select sales_revs Extract(Year from Year) as revs_year from bike_case;--repair_phones other data
-SELECT DATEDIFF('2017-01-13','2017-01-03') AS DateDiff;
+select sales_revs Extract(Year from Year) as revs_year from bike_case;
 
- 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+--sample exes
 --Find the Employees who hired in the Last n months.
 --inding the Employees who have been hire in the last n months. 
 --Here we get desire output by using TIMESTAMPDIFF() mysql function.
