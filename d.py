1e94275ea76e7ef10fa7a1b7971b080642344748
simofleanta <simo.fleanta@gmail.com>
2020-11-26 14:41:19 +0100

    s

--- d.py
+++ d.py
@@ -7,30 +7,26 @@ import time
 with open('datelazi.json', 'r') as file:
     s=json.load(file)
 
-print(type(s))
-
-
-
 #extract info from the nested json
 
 """print(s['charts']['dailyStats']['contains'])
-print(s['currentDayStats'])
+print(s['C'])
 print(s['currentDayStats']['numberCured'])
 print(s['currentDayStats']['numberInfected'])
 print(s['currentDayStats']['numberDeceased'])
 print(s['currentDayStats']['percentageOfMen'])
 print(s['currentDayStats']['percentageOfWomen'])
 print(s['currentDayStats']['distributionByAge'])
-print(s['currentDayStats']['countyInfectionsNumbers'])"""
+print(s['C']['countyInfectionsNumbers'])"""
 
 
-
-with open('datelazi.json') as file:
+with open('stats.json') as file:
     for line in file:
         data=json.loads(line)
     ww=pd.DataFrame(data)
-print(ww)
-
+df=pd.read_json("datelazi.json")
+bn=pd.DataFrame(df.features.values.tolist())['lasUpdatedOn']
+print(df.features.values.tolist())
 
     
 
