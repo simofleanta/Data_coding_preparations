022647bc8bf7fb890e75bfe01ea162005eba16cb
simofleanta <simo.fleanta@gmail.com>
2020-11-30 13:13:17 +0100

    s

---
+++ eco.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+
+economic=pd.read_csv('unemployment.csv')
+print(economic.columns)
+df_economic=DataFrame(economic.head(10))
+print(df_economic.head(10))
+
