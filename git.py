ded15b201694d0149049351a0c29c28a206c5f5d
simofleanta <simo.fleanta@gmail.com>
2020-11-30 17:17:46 +0100

    save

---
+++ git.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+
+
+git=pd.read_csv('bike_business_plan.csv')
+print(git.columns)
+df=DataFrame(git.head(20))
+print(df.head(20))
+
