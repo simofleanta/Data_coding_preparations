4a949f9309c1e44926c3121ed9952d28848ff962
simofleanta <simo.fleanta@gmail.com>
2020-11-30 17:46:50 +0100

    save

--- git.py
+++ git.py
@@ -16,21 +16,29 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 import plotly.express as px
 
+#pening a file
 git=pd.read_csv('GitHub_data.csv')
 print(git.columns)
 df=DataFrame(git.head(20))
 print(df.head(20))
 
+#making sense of data 
 c=df.dtypes
 c_missing=df.isnull().sum()
 print(c_missing)
+x=c.describe()
+#print(x)
+x_describe=df.describe()
+print(x_describe)
+x_shape=df.shape
+print(x_shape)
 
 #just in case need to encode numerical 
 #encoder=LabelEncoder()
 #numerical=df['date']=encoder.fit_transform(df['date'])
 
 xdf=df[['topic','projects','contributers','name','user','star','fork','issue','License','commits']].copy()
-print(xdf)
+#print(xdf)
 
 
 
