28e3f8aca0808c4a6f432f2c3a3a5853f08d2a23
simofleanta <simo.fleanta@gmail.com>
2020-12-01 11:04:02 +0100

    save

---
+++ git2020.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+
+#pening a file
+git=pd.read_csv('GitHub_data.csv')
+#print(git.columns)
+df=DataFrame(git.head(1000))
+#print(df.head(20))
+
+#data view
+c=df.dtypes
+c_missing=df.isnull().sum()
+#print(c_missing)
+x=c.describe()
+#print(x)
+x_describe=df.describe()
+#print(x_describe)
+x_shape=df.shape
+#print(x_shape)
+
+#just in case need to encode numerical 
+#encoder=LabelEncoder()
+#numerical=df['date']=encoder.fit_transform(df['date'])
+
+#index rename
+df = df.rename(columns={'Unnamed: 0': 'index', 'Unnamed: 0.1': 'sub_index'})
+df.drop('index', axis=1, inplace=True)
+df.reset_index(drop=True, inplace=True)
+
+info=df.info()
+print(info)
+print(df.columns)
+
+#get rid of str 5000+
+x=df.at[700, 'issue'] = str(5000)
+
+Numerical = ["star","fork","watch","issue","pull_requests","projects","commits","branches","packages","releases","contributers"]
+df[Numerical] = df[Numerical].fillna(0)
+df["issue"] = df["issue"].apply(lambda x: x.replace(',', '') if ',' in x else x).astype(float)
+df["pull_requests"] = df["pull_requests"].apply(lambda x: x.replace(',', '') if ',' in x else x).astype(float)
+df["commits"] = df["commits"].apply(lambda x: x.replace(',', '') if ',' in x else x).astype(float)
+df["branches"] = df["branches"].apply(lambda x: x.replace(',', '') if ',' in x else x).astype(float)
+df["contributers"] = df["contributers"].apply(lambda x: x.replace(',', '') if ',' in x else x).astype(float)
+
+
+df['star'] = df['star'].apply(lambda x: float(x.replace('k',''))*1000 if 'k' in x else x)
+df['fork'] = df['fork'].apply(lambda x: float(x.replace('k',''))*1000 if 'k' in x else x)
+df['watch'] = df['watch'].apply(lambda x: float(x.replace('k',''))*1000 if 'k' in x else x)
+
+
+
+
+
+
+
+
+
+"""#Making sense of data
+
+xdf=df[['topic','projects','contributers','name','user','star','fork','issue','License','commits']].copy()
+print(xdf)
+
+#how many projects are per topic?
+git_topic=xdf.groupby(['topic'])[['projects']]
+print(git_topic.count())
+
+#how many projects in total?
+proj_count=xdf.groupby(['projects'])
+print(proj_count.count())
+
+#how many contribs per topic?
+git_contribs=xdf.groupby(['topic'])[['contributers']]
+print(git_contribs.count())
+
+#how many contribs in projects
+contribs_projects=xdf.groupby(['topic','projects'])[['contributers']]
+print(contribs_projects.count())
+
+#star is counted in k 
+git_star=xdf.groupby(['projects'])[['star']]
+print(git_star.mean())
+
+git_name_star=xdf.groupby(['topic','name'])[['star']]
+print(git_name_star.sum())
+
+#forks are in k too 
+git_license=xdf.groupby(['topic','License'])[['fork']]
+print(git_license.sum())"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
