be155b362ad4716ab335be59b4d9394ba4e063b1
simofleanta <simo.fleanta@gmail.com>
2020-12-05 10:25:30 +0100

    s

--- prog_to read.py
+++ prog_to read.py
@@ -2,6 +2,16 @@
 
 import pandas as pd
 #open file
+df=pd.read_csv("GitHub_data.csv")
+#show the type of the df
+print(type(df))
+#print it just the first 3 rows
+print(df.head(3))
+#convert pandas df to a numpy array
+array=df.to_numpy()
+print(array)
+
+#open file
 df=pd.read_csv("Inflation_forecast.csv")
 #show the type of the df
 print(type(df))
@@ -10,5 +20,9 @@ print(df.head(3))
 #convert pandas df to a numpy array
 array=df.to_numpy()
 print(array)
+
+
+
+
 
 
