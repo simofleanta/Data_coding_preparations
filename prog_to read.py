fde044220e2f5c48cdf0b160599701aa31f4dbb4
simofleanta <simo.fleanta@gmail.com>
2020-12-05 10:22:32 +0100

    s

--- prog_to read.py
+++ prog_to read.py
@@ -1,3 +1,14 @@
+#create a python program to read csv without csv module
+
 import pandas as pd
+#open file
 df=pd.read_csv("Inflation_forecast.csv")
+#show the type of the df
 print(type(df))
+#print it just the first 3 rows
+print(df.head(3))
+#convert pandas df to a numpy array
+array=df.to_numpy()
+print(array)
+
+
