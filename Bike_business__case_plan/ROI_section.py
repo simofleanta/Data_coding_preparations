b40193c4cf5a95576bb9bf59ca2ee5af9f4f3737
simofleanta <simo.fleanta@gmail.com>
2020-12-06 19:32:04 +0100

    s

--- Bike_business__case_plan/ROI_section.py
+++ Bike_business__case_plan/ROI_section.py
@@ -25,13 +25,31 @@ print(df.head(500))
 """ROI ON 2020 in a pandemic it was anticipated a larger use of echo transport including bikes 
 instead of public transport so the investment was higher"""
 
+#2019 ROI
+Year2019=df[df.Year==2019]
+
 investment=40000 #received investment 
-bike_costs=Item_cost_month=df['Item_cost_month']
-loss=Loss_item=df['Loss_item']
+bike_costs=Item_cost_month=Year2019['Item_cost_month']
+loss=Loss_item=Year2019['Loss_item']
 
 net_profit=bike_costs*12-loss
-def ROI(investment,bike_costs,loss):
+def ROI_2019(investment,bike_costs,loss):
     return net_profit/investment*100
+print(ROI_2019(investment,bike_costs,loss))
+
+#roi in 2020 
+Year2020=df[df.Year==2020]
+investment=40000 #received investment 
+bike_costs=Item_cost_month=Year2020['Item_cost_month']
+loss=Loss_item=Year2020['Loss_item']
+
+net_profit=bike_costs*12-loss
+def ROI_2020(investment,bike_costs,loss):
+    return net_profit/investment*100
+print(ROI_2020(investment,bike_costs,loss))
+
+#roi/item 2019
+
 
 
 """def roi(investment,bike_costs,loss):
