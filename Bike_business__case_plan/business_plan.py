4f780a017706f91dcde17d23458f972ec0d95f7d
simofleanta <simo.fleanta@gmail.com>
2020-12-06 19:46:59 +0100

    finished the ROI section

--- Bike_business__case_plan/business_plan.py
+++ Bike_business__case_plan/business_plan.py
@@ -239,30 +239,61 @@ sns.heatmap(df.corr(),annot=True,cmap='Blues_r',mask=np.triu(df.corr(),k=1))
 """ROI ON 2020 in a pandemic it was anticipated a larger use of echo transport including bikes 
 instead of public transport so the investment was higher"""
 
-investment=65000
-bike_costs=2700
-loss=5800
+#2019 ROI
+#filtering the year
+Year2019=df[df.Year==2019]
+investment=40000 #received investment 
+#passing vriables to the desired columns 
+bike_costs=Item_cost_month=Year2019['Item_cost_month']
+loss=Loss_item=Year2019['Loss_item']
 
-def roi(investment,bike_costs,loss):
-    net_prof=bike_costs*12-loss
-    roi=(net_prof/investment*100)
-    return roi
+#finding out the netprofit
+net_profit=bike_costs*12-loss
 
-ROI=roi(investment,bike_costs,loss)
-print(ROI)
+def ROI_2019(investment,bike_costs,loss):
+    """function generating ROI for 2019"""
+    return net_profit/investment*100
+print(ROI_2019(investment,bike_costs,loss))
 
-#on 2019
-investment=40000
-bike_costs=1000
-loss=700
+#roi/item-Raleigh 2019
+#filtering the desired item in 2019
+Raleigh=Year2019[Year2019.Item=='Raleigh']
+investment=40000 #received investment 
+bike_costs=Item_cost_month=Raleigh['Item_cost_month']
+loss=Loss_item=Raleigh['Loss_item']
 
-def roi(investment,bike_costs,loss):
-    net_prof=bike_costs*12-loss
-    roi=(net_prof/investment*100)
-    return roi
+#finding out the netprofit 
+net_profit=bike_costs*12-loss
 
-ROI=roi(investment,bike_costs,loss)
-print(ROI)
+def ROI_Ral(investment,bike_costs,loss):
+    """Generating ROI for an item """
+    return net_profit/investment*100
+print(ROI_Ral(investment,bike_costs,loss))
+
+#roi in 2020 
+Year2020=df[df.Year==2020]
+investment=40000 #received investment 
+bike_costs=Item_cost_month=Year2020['Item_cost_month']
+loss=Loss_item=Year2020['Loss_item']
+
+net_profit=bike_costs*12-loss
+def ROI_2020(investment,bike_costs,loss):
+    return net_profit/investment*100
+print(ROI_2020(investment,bike_costs,loss))
+
+#roi item 2020
+Year2020=df[df.Year==2020]
+Orbea=Year2020[Year2020.Item=='Orbea']
+
+investment=40000 #received investment 
+bike_costs=Item_cost_month=Orbea['Item_cost_month']
+loss=Loss_item=Orbea['Loss_item']
+
+net_profit=bike_costs*12-loss
+def ROI_Orbea(investment,bike_costs,loss):
+    return net_profit/investment*100
+print(ROI_Orbea(investment,bike_costs,loss))
+
 #------------------------------------A/B-------------------------------------------------
 """Given the differences between the hears, it worth using an A/B approach over ites in years or season/months """
 
