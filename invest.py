9719f45ac3996095b4e5095d4ebaa89e33d9be7b
simofleanta <simo.fleanta@gmail.com>
2021-01-02 09:29:27 +0100

    add file on investment startup funding

---
+++ invest.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+
+
+investing=pd.read_csv('funding.csv')
+print(investing.columns)
+df=DataFrame(investing.head(500))
+print(df.head(500))
+
