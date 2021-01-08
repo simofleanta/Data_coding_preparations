05bd1b2f6e884f9f56473d12a449f9926e0d195d
simofleanta <simo.fleanta@gmail.com>
2021-01-08 09:34:10 +0100

    added file

---
+++ venture_overview.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+
+
+
+finance=pd.read_csv('venture_overview.csv')
+print(finance.columns)
+df_v=DataFrame(finance.head(120))
+print(df_v.head(120))
+
