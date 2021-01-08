85855c246f665b2e47ccd4190cbf251d0b27d417
simofleanta <simo.fleanta@gmail.com>
2021-01-08 16:52:27 +0100

    s

--- venture_overview.py
+++ venture_overview.py
@@ -46,38 +46,46 @@ Year2017=df_v[df_v.Year==2017]
 
 #plt.title('Gross margins')
 #plt.plot(Year2019.Month, Year2019.Total_gross_sales, label='Total_gross_sales')
-#plt.plot(Year2019.Month, Year2019.Sales_Margin, label='Sales_Margin')
 #plt.legend()
-#plt.show()
 
-#scatter
-#plt.scatter(df_v.Gross_Margin, df_v.Total_gross_sales, cmap='Blues', edgecolors='k', alpha=0.55)
-#plt.show()
-
-#boxplot with 2 vars
-#sns.boxplot(x='Year',y='Sales_Gross', data=df_v)
-#plt.show()
-
-#pairplot = sns.pairplot(Year2019, vars=['Sales_Gross','Gross_Margin'])
-#plt.show()
 
 
 #scatter dashboard on sales gross and total sales in 4 years 
-f,axes = plt.subplots(2,2, figsize=(15,13))
-axes[0,0].scatter(Year2020.Sales_Gross, Year2020.Total_gross_sales, cmap='summer', edgecolors='k',\
+f,axes = plt.subplots(2,2, figsize=(15,20))
+axes[0,0].scatter(df_v.Gross_Margin, df_v.Total_gross_sales, cmap='summer', edgecolors='k',\
      alpha=0.55)
 
-axes[0,1].scatter(Year2019.Sales_Gross, Year2019.Total_gross_sales, cmap='viridis', edgecolors='k', \
-    alpha=0.55)
+k1=sns.stripplot(x='Year', y='Gross_Margin',jitter=0.25, alpha=0.5, edgecolor='black',palette="Blues", data=df_v, \
+    ax=axes[0,1])
 
-axes[1,0].scatter(Year2018.Sales_Gross, Year2018.Total_gross_sales, cmap='ice', edgecolors='k', \
-    alpha=0.55)
+axes[1,0].plot(Year2019.Month, Year2019.Sales_Margin, label='Sales_Margin')
 
-axes[1,1].scatter(Year2017.Sales_Gross, Year2017.Total_gross_sales, cmap='husl', edgecolors='k', \
-    alpha=0.55)
-
+k2=sns.heatmap(df_v.corr(), annot=True, cmap='Blues',vmin=-1,vmax=1, yticklabels=False, ax=axes[1,1])
 
 plt.show()
+
+
+"""
+#polar chart on gross analysis 
+df = px.data.wind()
+figu = px.scatter_polar(df_v , r="Total_gross_sales", theta="Gross_Margin",symbol='Sales_Gross', template='plotly_dark',
+                        color_discrete_sequence=px.colors.sequential.Blues)
+plotly.offline.plot(figu, filename='M')
+
+
+
+#polar costs
+
+#open costs
+finance=pd.read_csv('costs.csv')
+print(finance.columns)
+df_c=DataFrame(finance.head(100))
+
+import plotly.express as px
+#df = px.data.wind()
+#figu = px.scatter_polar(df_c , r="Product_px", theta="Sale_rev",
+                       #color_continuous_scale='Blues')
+#plotly.offline.plot(figu, filename='M')"""
 
 
 
