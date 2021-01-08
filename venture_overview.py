d0a8ec71df1b9de4464558f06d7cf028f5232b75
simofleanta <simo.fleanta@gmail.com>
2021-01-08 15:09:44 +0100

    s

--- venture_overview.py
+++ venture_overview.py
@@ -19,17 +19,16 @@ import plotly.express as px
 #open file
 finance=pd.read_csv('Margin_Analysis.csv')
 print(finance.columns)
-df_v=DataFrame(finance.head(120))
-print(df_v.head(120))
+df_v=DataFrame(finance.head(100))
+print(df_v.head(10))
 
 #extract year 2020
 Year2020=df_v[df_v.Year==2020]
 
 #extract 2019
 Year2019=df_v[df_v.Year==2019]
-
-
-
+Year2018=df_v[df_v.Year==2018]
+Year2017=df_v[df_v.Year==2017]
 
 
 #plotly pie
@@ -62,17 +61,21 @@ Year2019=df_v[df_v.Year==2019]
 #pairplot = sns.pairplot(Year2019, vars=['Sales_Gross','Gross_Margin'])
 #plt.show()
 
-#scatter
-plt.scatter(Year2020.Gross_Margin, Year2020.Total_gross_sales, cmap='Blues', edgecolors='k', alpha=0.55)
-plt.show()
 
+#scatter dashboard on sales gross and total sales in 4 years 
+f,axes = plt.subplots(2,2, figsize=(15,13))
+axes[0,0].scatter(Year2020.Sales_Gross, Year2020.Total_gross_sales, cmap='summer', edgecolors='k',\
+     alpha=0.55)
 
-f,axes = plt.subplots(2,2, figsize=(15,15))
-axes[0,0].scatter(df_v.Gross_Margin, df_v.Total_gross_sales, cmap='Blues', edgecolors='k', alpha=0.55)
-axes[0,1].scatter(Year2019.Sales_Gross, Year2019.Total_gross_sales, cmap='Blues', edgecolors='k', alpha=0.55)
+axes[0,1].scatter(Year2019.Sales_Gross, Year2019.Total_gross_sales, cmap='viridis', edgecolors='k', \
+    alpha=0.55)
 
-k2=sns.boxplot(x='Year',y='Sales_Gross', data=df_v,\
-    ax=axes[1,1])
+axes[1,0].scatter(Year2018.Sales_Gross, Year2018.Total_gross_sales, cmap='ice', edgecolors='k', \
+    alpha=0.55)
+
+axes[1,1].scatter(Year2017.Sales_Gross, Year2017.Total_gross_sales, cmap='husl', edgecolors='k', \
+    alpha=0.55)
+
 
 plt.show()
 
