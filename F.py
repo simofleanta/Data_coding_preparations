2923e5c2172da290ab2c90ecfd69caad4c5ff8b2
simofleanta <simo.fleanta@gmail.com>
2021-01-11 13:34:15 +0100

    save time series

--- F.py
+++ F.py
@@ -15,13 +15,36 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 import plotly.express as px
-
+import datetime
 
 
 c=pd.read_csv('Energy_consumption.csv')
 print(c.columns)
 df_c=DataFrame(c.head(31))
 print(df_c.head(31))
+
+df_c['Date']=pd.to_datetime(df_c['Date'], infer_datetime_format=True)
+indexeddf=df_c.set_index(['Date'])
+print(indexeddf)
+
+#parsing to time format and extracting dates with 'created_at'
+
+x=df_c['Date']=pd.to_datetime(df_c['Date'], format='%y-%m-%d')
+
+Day=df_c['Date'].dt.day_name()
+print(Day)
+
+Month=df_c['Date'].dt.month_name()
+print(Month)
+
+Year=df_c['Date'].dt.year
+print(Year)
+
+
+
+
+
+
 
 
 
