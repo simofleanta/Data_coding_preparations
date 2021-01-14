c49d1792a3f309fe35a0f1389c589ab2f259033f
simofleanta <simo.fleanta@gmail.com>
2021-01-14 18:34:35 +0100

    data eas assessed

--- udacity.py
+++ udacity.py
@@ -17,11 +17,29 @@ from sklearn.metrics import r2_score
 import plotly.express as px
 import datetime
 
+"""In my Udacity Data Science Challenge, I was supposed to practice Python and create some data visuals using pandas data frames, with data. The aim of 
+the exercise was to go through basic data types, data frames and juggling with the data so obtain a small data analysis on random data
+
+So this ex will go through:
+opening file
+checking data types and making sense of the data
+date timeseries parsing
+one or 2 carts 
+subplots 
+"""
 
 u=pd.read_csv('udacity.csv')
 print(u.columns)
 udacity=DataFrame(u.head(60))
 print(udacity.head(60))
+
+#check dtypes
+print(udacity.dtypes)
+# if we hve null values 
+print(udacity.isnull)
+print(udacity.shape)
+print(udacity.describe())
+
 
 udacity['year']=pd.to_datetime(udacity['year'], infer_datetime_format=True)
 indexeddf=udacity.set_index(['year'])
@@ -45,8 +63,7 @@ udacity['Month']=udacity['year'].dt.month_name()
 udacity['Day']=udacity['year'].dt.day_name()
 print(udacity.head(31))
 
-#check dtypes
-print(udacity.dtypes)
+#operate
 
 
 
