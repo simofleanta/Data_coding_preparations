910f806ca513bdd6ad406b29cb30fe6c4b3fe55a
simofleanta <simo.fleanta@gmail.com>
2021-01-18 19:29:20 +0100

    s

--- bike_distance.py
+++ bike_distance.py
@@ -23,3 +23,31 @@ print(bike.columns)
 bike_df=DataFrame(bike.head(60))
 print(bike_df.head(60))
 
+#check dtypes
+print(bike_df.dtypes)
+# if we hve null values 
+print(bike_df.isnull)
+#shape of data
+print(bike_df.shape)
+#data description
+print(bike_df.describe())
+
+#parse index
+bike_df['year']=pd.to_datetime(bike_df['year'], infer_datetime_format=True)
+indexeddf=bike_df.set_index(['year'])
+print(indexeddf)
+
+#parsing to time format and extracting dates with 'created_at'
+x=bike_df['year']=pd.to_datetime(bike_df['year'], format='%d-%m-%y')
+
+Day=bike_df['year'].dt.day_name()
+Month=bike_df['year'].dt.month_name()
+Year=bike_df['year'].dt.year
+
+#subsetting 
+bike_df['Year']=bike_df['year'].dt.year
+bike_df['Month']=bike_df['year'].dt.month_name()
+bike_df['Day']=bike_df['year'].dt.day_name()
+
+
+
