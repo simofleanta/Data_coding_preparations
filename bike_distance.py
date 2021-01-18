2e73e1c4b2944211691d9d91e130bf503e70b42a
simofleanta <simo.fleanta@gmail.com>
2021-01-18 19:30:22 +0100

    s

--- bike_distance.py
+++ bike_distance.py
@@ -18,7 +18,7 @@ import plotly.express as px
 import datetime
 
 #open file
-bike=pd.read_csv('bike_blocks.csv')
+bike=pd.read_csv('Bikes_blocks.csv')
 print(bike.columns)
 bike_df=DataFrame(bike.head(60))
 print(bike_df.head(60))
@@ -48,6 +48,8 @@ Year=bike_df['year'].dt.year
 bike_df['Year']=bike_df['year'].dt.year
 bike_df['Month']=bike_df['year'].dt.month_name()
 bike_df['Day']=bike_df['year'].dt.day_name()
+
+
 
 
 
