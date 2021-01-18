19e351ae075bd51586b6aba19983dfd4468a96b7
simofleanta <simo.fleanta@gmail.com>
2021-01-18 19:12:28 +0100

    s

---
+++ bike_distance.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+import datetime
+
+#open file
+bike=pd.read_csv('bike_blocks.csv')
+print(bike.columns)
+bike_df=DataFrame(bike.head(60))
+print(bike_df.head(60))
+
