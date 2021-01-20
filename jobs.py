e4e9152d228f391f01da40486b6cc67e842e1cc2
simofleanta <simo.fleanta@gmail.com>
2021-01-20 19:30:09 +0100

    s

---
+++ jobs.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+import datetime
+
+#open file
+bis=pd.read_csv('BusinessAnalyst.csv')
+print(bis.columns)
+bis=DataFrame(bis.head(60))
+print(bis.head(60))
+
