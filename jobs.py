3575757d9918f0df4707a832f23be1e2d1494a88
simofleanta <simo.fleanta@gmail.com>
2021-01-21 22:08:57 +0100

    s

--- jobs.py
+++ jobs.py
@@ -31,18 +31,42 @@ print(business_analyst)
 
 business_intelligence=BA[BA.Job_Title=='Business_Intelligence_Analyst']
 print(business_intelligence)
+"""
+f,axes = plt.subplots(1,2, figsize=(15, 10))
+C=sns.scatterplot(business_intelligence.Salary_Estimate_k, business_intelligence.Rating, s=100, edgecolor='black', alpha=0.5,\
+     palette='Blues',ax=axes[0])
 
+D=sns.boxplot(business_intelligence.Salary_Estimate_k, business_intelligence.Sector, palette='viridis',hue_order=[True,False],ax=axes[1])
+
+plt.show()"""
+
+#sub2
+
+f,axes = plt.subplots(1,2, figsize=(15, 10))
+C=sns.scatterplot(business_analyst.Salary_Estimate_k, business_analyst.Rating, s=100, edgecolor='black', alpha=0.5,\
+     palette='Blues',ax=axes[0])
+
+D=sns.boxplot(business_analyst.Salary_Estimate_k, business_analyst.Sector, palette='viridis',hue_order=[True,False],ax=axes[1])
+
+plt.show()
+
+
+
+
+
+"""
 f,axes = plt.subplots(2,2, figsize=(9,9))
 K0=sns.scatterplot(business_intelligence.Salary_Estimate_k, business_intelligence.Rating, s=100, edgecolor='black', alpha=0.5,\
      palette='Blues',ax=axes[0,0])
 
-k1=sns.boxplot(business_intelligence.Salary_Estimate_k, business_intelligence.Sector, palette='viridis',hue_order=[True,False], ax=axes[0,1])
+k1=sns.scatterplot(business_analyst.Salary_Estimate_k, business_analyst.Rating, s=100, edgecolor='black', alpha=0.5,\
+     palette='Blues', ax=axes[0,1])
 
 k2=sns.boxplot(business_intelligence.Salary_Estimate_k, business_intelligence.Sector, palette='viridis',hue_order=[True,False],ax=axes[1,0])
 
 k3=sns.violinplot(x=business_analyst["Size"], y=business_analyst["Type_ownership"], palette="Blues", ax=axes[1,1])
 
-plt.show()
+plt.show()"""
 
 
 
