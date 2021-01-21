17f58c9f9f1813841f309df253eb256b77dffece
simofleanta <simo.fleanta@gmail.com>
2021-01-21 20:20:58 +0100

    s

--- jobs.py
+++ jobs.py
@@ -32,3 +32,19 @@ B=sns.scatterplot(BA.Founding_Year, BA.Salary_Estimate_k, s=100, edgecolor='blac
 
 plt.show()
 
+
+f,axes = plt.subplots(2,2, figsize=(15,30))
+K0=sns.scatterplot(BA.Size, BA.Rating, s=100, edgecolor='black', alpha=0.5,\
+     palette='Blues',ax=axes[0,0])
+
+k1=sns.violinplot(x=BA["Type_ownership"], y=BA["Salary_Estimate_k"], palette="Blues", ax=axes[0,1])
+
+k2=sns.boxplot(BA.Sector, BA.Salary_Estimate_k, palette='Blues',hue_order=[True,False],ax=axes[1,0])
+
+k3=sns.violinplot(x=BA["Type_ownership"], y=BA["Size"], palette="Blues", ax=axes[1,1])
+
+plt.show()
+
+
+sns.boxplot(BA.Sector, BA.Salary_Estimate_k, palette='Blues',hue_order=[True,False])
+plt.show()
