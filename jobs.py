ecbd4231d10e81d26894ef2d02fad8690a2d1363
simofleanta <simo.fleanta@gmail.com>
2021-01-21 20:29:37 +0100

    s

--- jobs.py
+++ jobs.py
@@ -22,15 +22,6 @@ print(jobs.columns)
 BA=DataFrame(jobs.head(60))
 print(BA.head(60))
 
-#How far the people travel to their work?
-f,axes = plt.subplots(1,2, figsize=(15, 10))
-A=sns.scatterplot(BA.Salary_Estimate_k, BA.Rating, s=100, edgecolor='black', alpha=0.5,\
-     palette='Blues',ax=axes[0])
-
-B=sns.scatterplot(BA.Founding_Year, BA.Salary_Estimate_k, s=100, edgecolor='black', alpha=0.5,\
-     palette='viridis',ax=axes[1])
-
-plt.show()
 
 
 f,axes = plt.subplots(2,2, figsize=(15,30))
@@ -39,12 +30,11 @@ K0=sns.scatterplot(BA.Size, BA.Rating, s=100, edgecolor='black', alpha=0.5,\
 
 k1=sns.violinplot(x=BA["Type_ownership"], y=BA["Salary_Estimate_k"], palette="Blues", ax=axes[0,1])
 
-k2=sns.boxplot(BA.Sector, BA.Salary_Estimate_k, palette='Blues',hue_order=[True,False],ax=axes[1,0])
+k2=sns.violinplot(x=BA["Salary_Estimate_k"], y=BA["Founding_Year"], palette="Blues",ax=axes[1,0])
 
-k3=sns.violinplot(x=BA["Type_ownership"], y=BA["Size"], palette="Blues", ax=axes[1,1])
+k3=sns.violinplot(x=BA["Size"], y=BA["Type_ownership"], palette="Blues", ax=axes[1,1])
 
 plt.show()
 
 
-sns.boxplot(BA.Sector, BA.Salary_Estimate_k, palette='Blues',hue_order=[True,False])
-plt.show()
+
