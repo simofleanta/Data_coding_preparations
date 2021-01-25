21b596a421df2bcc2cbc7a29c81c6c1250b5f7a4
simofleanta <simo.fleanta@gmail.com>
2021-01-25 18:46:02 +0100

    added subplot and corr chart

--- Joined_t.py
+++ Joined_t.py
@@ -16,3 +16,31 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 import plotly.express as px
 import datetime
+
+#open file
+j=pd.read_csv('joined_t.csv')
+print(j.columns)
+ba=DataFrame(j.head(21))
+print(ba.head(21))
+
+#scatter subplot
+f,axes = plt.subplots(1,2, figsize=(15, 10))
+A=sns.scatterplot(ba.Salary, ba.KPI, s=100, edgecolor='black', alpha=0.5,\
+     palette='Blues',ax=axes[0])
+
+B=fig2=sns.boxplot(ba.Location, ba.KPI, palette='viridis',hue_order=[True,False], ax=axes[1])
+
+plt.show()
+
+
+sns.heatmap(ba.corr(), annot=True, cmap='Blues', linewidth=1,vmin=-1,vmax=1, yticklabels=True,xticklabels=True)
+plt.show()
+
+
+
+
+
+
+
+
+
