942cee41c4a235bfcdf3b48d9840c16eea1abc8e
simofleanta <simo.fleanta@gmail.com>
2021-01-25 12:46:22 +0100

    s

--- SQL_basic syntax/inner_query.sql
+++ SQL_basic syntax/inner_query.sql
@@ -1,4 +1,4 @@
- select orderid,employeeid,shipperid,(select avg(customerid) where orderdate between orderdate-3 and orderdate),
+select orderid,employeeid,shipperid,(select avg(customerid) where orderdate between orderdate-3 and orderdate),
 (select avg(customerid) where orderdate between orderdate-2 and orderdate),
 (select max(customerid) where orderdate between orderdate-8 and orderdate),
 (select sum(customerid) where shipperid=3)
