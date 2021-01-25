bf83fb23b2c130b29f0eca7ebc594ff210cf8d9a
simofleanta <simo.fleanta@gmail.com>
2021-01-25 18:54:54 +0100

    charts done

--- Joined_t.py
+++ Joined_t.py
@@ -26,14 +26,13 @@ print(ba.head(21))
 #scatter subplot
 f,axes = plt.subplots(1,2, figsize=(15, 10))
 A=sns.scatterplot(ba.Salary, ba.KPI, s=100, edgecolor='black', alpha=0.5,\
-     palette='Blues',ax=axes[0])
+     palette='Blues',ax=axes[0]).set_title("Relationship between KPI and income")
 
-B=fig2=sns.boxplot(ba.Location, ba.KPI, palette='viridis',hue_order=[True,False], ax=axes[1])
+B=fig2=sns.boxplot(ba.Location, ba.KPI, palette='viridis',hue_order=[True,False], ax=axes[1]).set_title("KPI per Location")
 
 plt.show()
 
-
-sns.heatmap(ba.corr(), annot=True, cmap='Blues', linewidth=1,vmin=-1,vmax=1, yticklabels=True,xticklabels=True)
+sns.heatmap(ba.corr(), annot=True, cmap='Blues', linewidth=1,vmin=-1,vmax=1, yticklabels=True,xticklabels=True).set_title("Corelation heatmap")
 plt.show()
 
 
