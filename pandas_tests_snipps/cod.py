2e13140404d507977fc42190cebe6c8419ee1d3f
simofleanta <simo.fleanta@gmail.com>
2021-02-02 13:35:52 +0100

    s

---
+++ pandas_tests_snipps/cod.py
@@ -0,0 +1,27 @@
+"""def shape(q):
+    tree=2*q-2
+    for i in range(0,q):
+        for j in range(0,tree):
+            print(end="")
+        tree= tree -1
+        for j in range(0,i+1):
+            print("*", end='')
+        print("*")
+print(shape(5))
+"""
+
+factorial=1 #distance between 1 and 2+1(3)=2
+for i in range(1, 2+1):
+    factorial *= i
+print(i)
+
+
+
+
+
+
+
+
+
+
+
