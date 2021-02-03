0b52911e669d0b6e093548340e7982fdd31e1480
simofleanta <simo.fleanta@gmail.com>
2021-02-03 11:27:59 +0100

    s

--- m.py
+++ m.py
@@ -49,12 +49,42 @@ print(m)
 Month_index=indexeddf.resample('M')
 print(Month_index)
 
-
-
-
 # see sum for both months jan and feb
 Sum_M_index=indexeddf.resample('M').sum()
 print(Sum_M_index)
+
+#subplot 
+f,axes = plt.subplots(1,2, figsize=(15, 10))
+fig1=sns.violinplot(x=m["Month"], y=m["Actual_Revenue"], palette="viridis",ax=axes[0]).set_title("Monthly Actual Revenue")
+fig2=sns.boxplot(m.Marketing_Channel, m.Actual_Revenue, palette='Blues',hue_order='Month',ax=axes[1]).set_title("Actual Revenue per Channel")
+plt.show()
+
+###############
+
+#Trends
+
+#scatter subplot to see monthly trend and daily trend
+f,axes = plt.subplots(1,2, figsize=(15, 10))
+A=sns.scatterplot(m.Month, m.Actual_Revenue, s=100, edgecolor='black', alpha=0.5,\
+     palette='Blues',ax=axes[0]).set_title("Monthy Revenue scatter chart")
+
+B=sns.scatterplot(m.Day, m.Actual_Revenue, s=100, edgecolor='black', alpha=0.5,\
+     palette='viridis',ax=axes[1]).set_title("Daily Revenue trend chart")
+plt.show()
+
+
+#m to csv fro forecast in power bi
+df=m.to_csv('forecast.csv')
+
+
+
+
+
+
+
+
+
+
 
 
 
