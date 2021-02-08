3e33067dbcd0fa4d7d49ecf69a99a5dd5499346f
simofleanta <simo.fleanta@gmail.com>
2021-02-08 15:07:13 +0100

    added commentrs

--- codility training/fiboncci_sequence.py
+++ codility training/fiboncci_sequence.py
@@ -1,16 +1,20 @@
-
+#memoization to cache values 
+#use fib cache with {}
 fibonacci_cache={}
+
+
 def fibonacci (n):
+    """if I have cached a value then return the value"""
     if n in fibonacci_cache:
         return fibonacci_cache[n]
-    
+# perform the nth tem if cluase    
     if n==1:
         value=1
     elif n== 2:
         value=2
     elif n>2:
         value=fibonacci(n-1)+fibonacci(n-2)
-    # cache
+# cache the value and return it 
     fibonacci_cache[n]=value
     return value
 
