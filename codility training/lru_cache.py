a8ed67e4eccbc39356bec211e8e69f1a5ebcf51e
simofleanta <simo.fleanta@gmail.com>
2021-02-09 15:43:07 +0100

    s

--- codility training/lru_cache.py
+++ codility training/lru_cache.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-
+"""
 
 @lru_cache(maxsize=1000)
 def fibonacci(n):
@@ -12,9 +12,25 @@ def fibonacci(n):
 
 for n in range(1, 50):
     print((n+1) /fibonacci(n))
-
+"""
 
 
 
     
 
+def fibonacci(n):
+    if(n<=1):
+        return n
+    return fibonacci(n-1)+fibonacci(n-2)
+
+for n in range(1, 50):
+    print((n+1) /fibonacci(n))
+
+    
+
+
+
+
+
+
+        
