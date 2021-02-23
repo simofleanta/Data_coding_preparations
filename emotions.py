8133475256d8496d1797246e4d7d2732f508fc30
simofleanta <simo.fleanta@gmail.com>
2021-02-23 14:54:39 +0100

    s

---
+++ emotions.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+import datetime
+import time
+
+e=pd.read_csv('Focus_data.csv')
+print(e.columns)
+e_df=DataFrame(e.head(60))
+print(e_df.head(60))
