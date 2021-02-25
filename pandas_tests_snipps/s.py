24a359a0c25c46b5dc061b375df354ff43be40a2
simofleanta <simo.fleanta@gmail.com>
2021-02-25 10:12:35 +0100

    s

--- pandas_tests_snipps/s.py
+++ pandas_tests_snipps/s.py
@@ -1,10 +1,14 @@
 """sudoku"""
 
 def find_next_empty(puzzle):
-    pass
+    for r in range(9):
+        for c in range(9):
+            if puzzle[r][c] == -1:
+                return r, c
 
 
-
+    return None,None  #if there are no empty spaces in the puzzle (-1)
+    
 
 
 def sudoku (puzzle):
