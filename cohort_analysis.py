43f690195f5424496f34d971977703c5f4526b8d
simofleanta <simo.fleanta@gmail.com>
2021-03-01 11:28:13 +0100

    setting a new doc for  analysis

---
+++ cohort_analysis.py
@@ -0,0 +1,45 @@
+
+import pandas as pd
+import seaborn as sns 
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import plotly
+import statistics
+import plotly.express as px
+import stats
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+import plotly.express as px
+import datetime
+
+
+
+
+
+
+# open cohort
+
+c=pd.read_csv('cohort.csv')
+print(c.columns)
+cohort=DataFrame(c.head(1113))
+print(cohort.head(1113))
+
+#parse index
+cohort['year']=pd.to_datetime(cohort['year'], infer_datetime_format=True)
+indexeddf=cohort.set_index(['year'])
+print(indexeddf)
+
+#parsing to time format and extracting dates with 'created_at'
+x=cohort['year']=pd.to_datetime(cohort['year'], format='%d-%m-%y')
+
+Day=cohort['year'].dt.day_name()
+Month=cohort['year'].dt.month_name()
+Year=cohort['year'].dt.year
+
+
