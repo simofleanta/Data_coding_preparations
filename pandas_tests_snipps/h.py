c68d36f753328319d8930c263c1d040f36a86273
simofleanta <simo.fleanta@gmail.com>
2021-03-10 15:29:38 +0100

    s

--- pandas_tests_snipps/h.py
+++ pandas_tests_snipps/h.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import seaborn as sns 
 from pandas import DataFrame
@@ -20,12 +19,16 @@ import datetime
 import datetime as dt
 import time
 
+# Supress Scientific notation in python
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
+# Display all columns of long dataframe
+pd.set_option('display.max_columns', None)
 
 c='h.csv'
 cohort=pd.read_csv(c, encoding=('ISO-8859-1'), low_memory=False)
 #print(cohort.head(5))
 
-print(cohort.info())
 
 #describe data after cleaning nan and duplicated vals
 print(cohort.describe())
@@ -39,14 +42,8 @@ cohort['Date']=pd.to_datetime(cohort['Date'], infer_datetime_format=True)
 indexeddf=cohort.set_index(['Date'])
 #print(indexeddf)
 
-"""
-#parsing to time format and extracting dates with 'created_at'
-x=cohort['InvoiceDate']=pd.to_datetime(cohort['InvoiceDate'], format='%y-%m-%d')
 
-Day=cohort['InvoiceDate'].dt.day_name()
-Month=cohort['InvoiceDate'].dt.month
-Year=cohort['InvoiceDate'].dt.year"""
-
+################################################based on country code########################################################################
 
 # cohort analysis start
 
@@ -54,11 +51,13 @@ Year=cohort['InvoiceDate'].dt.year"""
 def get_month(x):
     return dt.datetime (x.year, x.month, 1)
 
-cohort['Rate_Month']=cohort['Date'].apply(get_month)
-grouping=cohort.groupby('Domain_code')['Rate_Month']
-cohort['Rate_Month']=grouping.transform('min')
+
+cohort['ClientMonth']=cohort['Date'].apply(get_month)
+grouping=cohort.groupby('Country_code')['ClientMonth']
+cohort['CohortMonth']=grouping.transform('min')
 
 print(cohort.tail())
+
 
 #extract days, months
 
@@ -67,26 +66,27 @@ def get_month_int(cohortframe, column):
     month=cohortframe[column].dt.month
     day=cohortframe[column].dt.day
     return year, month, day
+
 #call function 
-invoice_year, invoice_month,_=get_month_int(cohort,'Rate_Month')
-cohort_year, cohort_month,_=get_month_int(cohort, 'Rate_Month')
+client_year, client_month,_=get_month_int(cohort,'ClientMonth')
+cohort_year, cohort_month,_=get_month_int(cohort, 'CohortMonth')
 
 #create year an month diffs
-year_diff=invoice_year-cohort_year
-month_diff=invoice_month-cohort_month
+year_diff=client_year - cohort_year
+month_diff=client_month - cohort_month
 
 #create cohortindex
 cohort['CohortIndex']=year_diff * 12 + month_diff +1
 
 #count monthly active clients from month cohorts
 
-grouping = cohort.groupby(['Rate_Month', 'CohortIndex'])
-cohort_data = grouping['Domain_code'].apply(pd.Series.nunique)
+grouping = cohort.groupby(['CohortMonth', 'CohortIndex'])
+cohort_data = grouping['Country_code'].apply(pd.Series.nunique)
 
 
 #return number of unique vals
 cohort_data = cohort_data.reset_index()
-cohort_counts = cohort_data.pivot(index='Rate_Month', columns='CohortIndex', values='Domain_code')
+cohort_counts = cohort_data.pivot(index='CohortMonth', columns='CohortIndex', values='Client_Count')
 print(cohort_counts)
 
 
@@ -99,35 +99,19 @@ print(retention)
 
 plt.figure(figsize=(15,7))
 plt.title('Retention levels on monthly cohorts')
-sns.heatmap(data=retention, annot=True, fmt='.0%', vmin=0.0, vmax=0.5, cmap='Blues')
+sns.heatmap(data=retention, annot=True, linewidth = 1.7 ,fmt='.0%', vmin=0.0, vmax=0.5, cmap='Blues')
 plt.show()
 
-#mean quantity on cohorts
 
-grouping = cohort.groupby(['Rate_Month', 'CohortIndex'])
-cohort_data = grouping['Service_price'].mean()
-cohort_data=cohort_data.reset_index()
-avg_q=cohort_data.pivot(index='Rate_Month', columns='CohortIndex', values='Service_price')
-avg_q.round(1)
-avg_q.index=avg_q.index.date
-
-plt.figure(figsize=(15,7))
-plt.title('Service_price on monthly cohorts')
-sns.heatmap(data=avg_q, annot=True, vmin=0.0, vmax=20, cmap='YlOrRd')
-plt.show()
-
-#########################################################################
-"""
-#mean quantity on cohorts
 
 grouping = cohort.groupby(['CohortMonth', 'CohortIndex'])
-cohort_data = grouping['UnitPrice'].mean()
+cohort_data = grouping['Out_px'].mean()
 cohort_data=cohort_data.reset_index()
-avg_q=cohort_data.pivot(index='CohortMonth', columns='CohortIndex', values='UnitPrice')
-avg_q.round(1)
+avg_q=cohort_data.pivot(index='CohortMonth', columns='CohortIndex', values='Out_px')
+avg_q.round(2)
 avg_q.index=avg_q.index.date
 
 plt.figure(figsize=(15,7))
-plt.title('UnitPrice on monthly cohorts')
-sns.heatmap(data=avg_q, annot=True, vmin=0.0, vmax=20, cmap='PiYG')
-plt.show()"""
+plt.title('Avg_q on monthly cohorts')
+sns.heatmap(data=avg_q, annot=True, fmt='.1f',vmin='0.0', vmax=20, linewidth = 1.7, cmap='viridis')
+plt.show()
