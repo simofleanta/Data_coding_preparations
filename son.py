e0fded187443542f5d7a73d75c093218a68cff3a
simofleanta <simo.fleanta@gmail.com>
2021-03-16 12:02:53 +0100

    changed from cohort to cluster analysis

--- son.py
+++ son.py
@@ -21,60 +21,19 @@ import datetime as dt
 import time
 
 
-c='transactions.csv'
+c='Mall_Customers.csv'
 cohort=pd.read_csv(c, encoding=('ISO-8859-1'), low_memory=False)
-#print(cohort.head(5))
-
-print(cohort.info())
-
-#check in missing data
-print(cohort.isnull().sum())
-
-#clean missing data
-cohort=cohort.dropna(subset=['ID'])
-print(cohort.isnull().sum())
-
-#check for duplicate values and then clean
-print(cohort.duplicated().sum())
-
-#clean duplicated
-cohort=cohort.drop_duplicates()
-print(cohort.duplicated().sum())
-
-
-#describe data after cleaning nan and duplicated vals
-print(cohort.describe())
-
-#print the shape of data
-print(cohort.shape)
-
-#parse dates in index (definitely need it)
-
-cohort['Date']=pd.to_datetime(cohort['Date'], infer_datetime_format=True)
-indexeddf=cohort.set_index(['Date'])
-#print(indexeddf)
-
-
-#parsing to time format and extracting dates with 'created_at'
-x=cohort['Date']=pd.to_datetime(cohort['Date'], format='%y-%m-%d')
-
-Day=cohort['Date'].dt.day_name()
-Month=cohort['Date'].dt.month
-Year=cohort['Date'].dt.year
-
-#subsetting 
-cohort['Year']=cohort['Date'].dt.year
-cohort['Month']=cohort['Date'].dt.month
-cohort['Day']=cohort['Date'].dt.day
 #print(cohort)
 
-"""
 
-# Problems_solved distribution
 
-plt.rcParams['figure.figsize'] = (15, 8)
-sns.countplot(cohort['Problems_solved'], palette = 'Blues')
-plt.title('Distribution of problems solved', fontsize = 20)
+
+
+# distribution bar
+
+plt.rcParams['figure.figsize'] = (11, 5)
+sns.countplot(cohort['Annual Income (k$)'], palette = 'Blues')
+plt.title('Distribution of annual income', fontsize = 20)
 plt.show()
 
 
@@ -104,21 +63,40 @@ plt.show()
 km = KMeans(n_clusters = 5, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
 y_means = km.fit_predict(x)
 
-plt.scatter(x[y_means == 0, 0], x[y_means == 0, 1], s = 100, c = 'pink', label = 'miser')
+plt.scatter(x[y_means == 0, 0], x[y_means == 0, 1], s = 100, c = 'green', label = 'not_careful')
 plt.scatter(x[y_means == 1, 0], x[y_means == 1, 1], s = 100, c = 'yellow', label = 'general')
-plt.scatter(x[y_means == 2, 0], x[y_means == 2, 1], s = 100, c = 'cyan', label = 'target')
-plt.scatter(x[y_means == 3, 0], x[y_means == 3, 1], s = 100, c = 'magenta', label = 'spendthrift')
+plt.scatter(x[y_means == 2, 0], x[y_means == 2, 1], s = 100, c = 'red', label = 'target')
+plt.scatter(x[y_means == 3, 0], x[y_means == 3, 1], s = 100, c = 'blue', label = 'spendthrift')
 plt.scatter(x[y_means == 4, 0], x[y_means == 4, 1], s = 100, c = 'orange', label = 'careful')
-plt.scatter(km.cluster_centers_[:,0], km.cluster_centers_[:, 1], s = 50, c = 'blue' , label = 'centeroid')
+plt.scatter(km.cluster_centers_[:,0], km.cluster_centers_[:, 1], s = 50, c = 'cyan' , label = 'centeroid')
 
 plt.style.use('fivethirtyeight')
 plt.title('K Means Clustering', fontsize = 20)
-plt.xlabel('ID')
-plt.ylabel('Problems_solved')
+plt.xlabel('Annual Income')
+plt.ylabel('Spending pattern')
 plt.legend()
 plt.grid()
 plt.show()
-"""
+
+
+kmeans = KMeans(n_clusters = 4, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+ymeans = kmeans.fit_predict(x)
+
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.title('Age clusters', fontsize = 30)
+
+plt.scatter(x[ymeans == 0, 0], x[ymeans == 0, 1], s = 100, c = 'blue', label = 'Normal Customers' )
+plt.scatter(x[ymeans == 1, 0], x[ymeans == 1, 1], s = 100, c = 'lightgreen', label = 'Priority Customers')
+plt.scatter(x[ymeans == 2, 0], x[ymeans == 2, 1], s = 100, c = 'yellow', label = 'Target Customers(Young)')
+plt.scatter(x[ymeans == 3, 0], x[ymeans == 3, 1], s = 100, c = 'red', label = 'Target Customers(Old)')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 50, c = 'cyan')
+
+plt.style.use('fivethirtyeight')
+plt.xlabel('Age')
+plt.ylabel('Spending pattern')
+plt.legend()
+plt.grid()
+plt.show()
 
 
 
