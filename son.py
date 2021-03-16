d861f90f0d508ea468601efe7429bc1e7086c5f9
simofleanta <simo.fleanta@gmail.com>
2021-03-16 11:23:11 +0100

    s

--- son.py
+++ son.py
@@ -66,13 +66,57 @@ Year=cohort['Date'].dt.year
 cohort['Year']=cohort['Date'].dt.year
 cohort['Month']=cohort['Date'].dt.month_name()
 cohort['Day']=cohort['Date'].dt.day_name()
-print(cohort)
+#print(cohort)
 
 
+# Problems_solved distribution
+
+plt.rcParams['figure.figsize'] = (15, 8)
+sns.countplot(cohort['Problems_solved'], palette = 'Blues')
+plt.title('Distribution of problems solved', fontsize = 20)
+plt.show()
 
 
+#Clusters
 
 
+x = cohort.iloc[:, [3, 4]].values
+
+# let's check the shape of x
+print(x.shape)
+
+from sklearn.cluster import KMeans
+
+"""wcss = []
+for i in range(1, 11):
+    km = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    km.fit(x)
+    wcss.append(km.inertia_)
+    
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method', fontsize = 20)
+plt.xlabel('No. of Clusters')
+plt.ylabel('wcss')
+plt.show()"""
+
+
+km = KMeans(n_clusters = 5, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+y_means = km.fit_predict(x)
+
+plt.scatter(x[y_means == 0, 0], x[y_means == 0, 1], s = 100, c = 'pink', label = 'miser')
+plt.scatter(x[y_means == 1, 0], x[y_means == 1, 1], s = 100, c = 'yellow', label = 'general')
+plt.scatter(x[y_means == 2, 0], x[y_means == 2, 1], s = 100, c = 'cyan', label = 'target')
+plt.scatter(x[y_means == 3, 0], x[y_means == 3, 1], s = 100, c = 'magenta', label = 'spendthrift')
+plt.scatter(x[y_means == 4, 0], x[y_means == 4, 1], s = 100, c = 'orange', label = 'careful')
+plt.scatter(km.cluster_centers_[:,0], km.cluster_centers_[:, 1], s = 50, c = 'blue' , label = 'centeroid')
+
+plt.style.use('fivethirtyeight')
+plt.title('K Means Clustering', fontsize = 20)
+plt.xlabel('ID')
+plt.ylabel('Problems_solved')
+plt.legend()
+plt.grid()
+plt.show()
 
 
 
