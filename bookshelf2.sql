2107abab4dec389359c9e93e5fefea3ef7105369
simofleanta <simo.fleanta@gmail.com>
2021-05-14 09:34:39 +0200

    s

---
+++ bookshelf2.sql
@@ -0,0 +1,13 @@
+create table BookShelf2 (Author CHAR (25),
+                      Title char(25),
+                      Editions char(25),
+                      TotalinDeposit char(25),
+                      EditionsCost int,
+                      Borrowed int,
+                      BorrowedCosts int,
+                      Onshelf int,
+                      OnshelfCosts int,
+                      BookCost int,
+                      TotalBookCost int);
+                      
+                      
