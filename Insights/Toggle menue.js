e768fe542c9d14b71db26fc54c93a4c3b59a185b
simofleanta <simo.fleanta@gmail.com>
2021-05-29 12:15:20 +0200

    s

--- Insights/Toggle menue.js
+++ Insights/Toggle menue.js
@@ -2,10 +2,10 @@
 var navLinks = document.getElementById("navLinks");
 
 function showMeneu(){
-     navLinks.style.right ="0"};
+     navLinks.style.right ="0"}
+
 function hideMeneu(){
     navLinks.style.right ="-350px"
-};
-
+}
  
 </script>
